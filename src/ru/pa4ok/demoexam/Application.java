b1f6dc33e50b6a5f6989742c092c07a4b37d964a
Pa4ok <n_leontiev@mail.ru>
2021-09-01 14:42:35 +0300

    01.09.21 ссылочные типы, методы

--- src/ru/pa4ok/demoexam/Application.java
+++ src/ru/pa4ok/demoexam/Application.java
@@ -1,6 +1,7 @@
 package ru.pa4ok.demoexam;
 
 import java.util.Arrays;
+import java.util.Random;
 
 /**
  * shift + F10 - запуск последней конфигурации
@@ -19,7 +20,7 @@ public class Application
     {
         //System.out.println("hello fspo");
 
-        int i1 = 2_000_000_000;
+        /*int i1 = 2_000_000_000;
         float f = 3.5F;
         double d = 455.4324D;
         boolean b = true;
@@ -31,9 +32,9 @@ public class Application
         Double double1;
         Boolean boolean1;
         Character character;
-        Long long1;
+        Long long1;*/
 
-        String s = "e23ijfrowefg";
+        //String s = "e23ijfrowefg";
         /*System.out.println(s.endsWith("fg"));
         System.out.println(s.length());
         System.out.println(s.substring(0, 4));
@@ -71,5 +72,76 @@ public class Application
         /*System.out.println(intArr2);
         System.out.println(Arrays.toString(intArr2));*/
 
+        //int[] arr = {3, 5, -5, 53, 5, 2, 9, 1};
+        /*System.out.println(Arrays.toString(arr));
+        for(int i=0; i<arr.length-1; i++) {
+            for(int j=i; j<arr.length; j++) {
+                if(arr[i] > arr[j]) {
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+        */
+
+        /*String ss = """
+                SELECT * FROM table
+                WHERE ... = ...
+                """;*/
+
+        /*int[] arr = {3, 5, -5, 53, 5, 2, 9, 1};
+        printArr(arr);
+        sortArray(arr);
+        printArr(arr);*/
+
+        /*int i = 10;
+        System.out.println(i);
+        test(i);
+        System.out.println(i);*/
+
+        Random rand = new Random();
+        int i = rand.nextInt();
+        int j = rand.nextInt(1000);
+        System.out.println(i + " " + j);
+
+        /*
+            реализовать и протестировать
+            метод рандомно заполняющий массив
+            метод сортирующий массив
+            метод приравнивает все отрицательные элементы 0
+         */
+    }
+
+    public static void printArr(int[] arr)
+    {
+        String s = "[";
+        for(int i=0; i<arr.length; i++) {
+            s += arr[i];
+            if(i != arr.length-1) {
+                s += ", ";
+            }
+        }
+        s += "]";
+        System.out.println(s);
+    }
+
+    public static void sortArray(int[] arr)
+    {
+        for(int i=0; i<arr.length-1; i++) {
+            for(int j=i; j<arr.length; j++) {
+                if(arr[i] > arr[j]) {
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+    }
+
+    public static void test(int i)
+    {
+        i *= 2;
     }
 }
