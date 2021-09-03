9bd3604cd86b0c21818fb0d5aa6d0ebd4074eb93
Pa4ok <n_leontiev@mail.ru>
2021-09-03 14:57:51 +0300

    функции, строки

--- src/App.java
+++ src/App.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 /**
  * shift + F10 - запуск последней конфигурации
  */
@@ -14,7 +16,7 @@ public class App
         long l = 1_000_000_000_000_000_000L;*/
 
         //int[] arr1; //null
-        int intValue = 10;
+        /*int intValue = 10;
         int[] arr1 = new int[10];
         int[] arr2 = {1, 2, 3, 4, 5, 6, 7};
 
@@ -33,7 +35,7 @@ public class App
         for(int i=0; i<arr1.length; i++) {
             System.out.printf(arr1[i] + " ");
         }
-        System.out.println();
+        System.out.println();*/
 
         /*
             создать массив int
@@ -41,6 +43,68 @@ public class App
             сделать функцию которая будет сортировать этот массив
             сделать функцию которая будет выводить массив в консоль 1 строчкой
          */
+
+        int[] arr = new int[10];
+        /*printArray(arr);
+        fillArray(arr);
+        printArray(arr);
+        sortArray(arr);
+        printArray(arr);
+        */
+
+        /*fillArray(arr);
+        for(int i : arr) {
+            System.out.println(i);
+        }*/
+
+        /*String s = "32432o4h2hEFEWF32f";
+        System.out.println(s.substring(3, 8));
+        System.out.println(s.length());
+        System.out.println(s.toLowerCase());
+        System.out.println(s.toUpperCase());
+        System.out.println(s.endsWith("32f"));
+        System.out.println(s.charAt(8));
+
+        s += "eflmewfew";
+        s += '%';
+
+        System.out.println(s);*/
+
+        //java 15+
+        String textBlock = """
+                <html>
+                    ...
+                </html>
+                """;
+    }
+
+    public static void fillArray(int[] arr)
+    {
+        Random rand = new Random();
+        for(int i=0; i<arr.length; i++) {
+            arr[i] = rand.nextInt(1000);
+        }
+    }
+
+    public static void printArray(int[] arr)
+    {
+        for(int i=0; i<arr.length; i++) {
+            System.out.printf(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void sortArray(int[] arr)
+    {
+        for(int i=0; i<arr.length-1; i++) {
+            for(int j=i; j<arr.length; j++) {
+                if(arr[i] > arr[j]) {
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
     }
 
     public static void test(int[] arr)
