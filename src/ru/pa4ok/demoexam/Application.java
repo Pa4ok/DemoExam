37ff1f1753aaf49c09e23dbdf0a5b7957b58f9b2
Pa4ok <n_leontiev@mail.ru>
2021-09-03 12:05:00 +0300

    03.09.21 классы, ссылочные типы данных

--- src/ru/pa4ok/demoexam/Application.java
+++ src/ru/pa4ok/demoexam/Application.java
@@ -1,8 +1,5 @@
 package ru.pa4ok.demoexam;
 
-import java.util.Arrays;
-import java.util.Random;
-
 /**
  * shift + F10 - запуск последней конфигурации
  * alt + enter - вызов контекстного меню по исправлению ошибки
@@ -11,163 +8,61 @@ import java.util.Random;
 
 public class Application
 {
-    /*
-        создать int массив и отсортировать его любым алгоритмом
-        +вывод массива до и после сортировки
-     */
-
     public static void main(String[] args)
     {
-        //System.out.println("hello fspo");
+        Book book = new Book("my title 1", 321);
 
-        /*int i1 = 2_000_000_000;
-        float f = 3.5F;
-        double d = 455.4324D;
-        boolean b = true;
-        char c = 'f';
-        long l = 4_444_444_444_444_444L;
+        /*System.out.println(book);
+        book.print();
 
-        Integer integer; //null
-        Float float1 = 3.45F;
-        Double double1;
-        Boolean boolean1;
-        Character character;
-        Long long1;*/
+        Book book1 = new Book("second book");
+        book1.print();*/
 
-        //String s = "e23ijfrowefg";
-        /*System.out.println(s.endsWith("fg"));
-        System.out.println(s.length());
-        System.out.println(s.substring(0, 4));
-        System.out.println(s.charAt(5));
-        System.out.println(s.toLowerCase());
-        System.out.println(s.toUpperCase());
-        */
-
-        /*for(int i=0; i<10; i++) {
-            System.out.printf(i + " ");
-        }*/
-
-        //int[] intArr; //null
-        int[] intArr1 = new int[10];
-        int[] intArr2 = {1, 2, 3, 4, 5, 6, 7};
-
-        /*for (int i = 0; i < intArr1.length; i++) {
-            System.out.printf(intArr1[i] + " ");
-        }
-        System.out.println();
-
-        for (int i = 0; i < intArr2.length; i++) {
-            System.out.printf(intArr2[i] + " ");
-        }
-        System.out.println();*/
-
-        for(int i2 : intArr2) {
-            /*if(i2 % 2 == 0) {
-                System.out.println(i2);
-            }*/
-
-            //System.out.println(i2 % 2 == 0 ? i2 : -5);
-        }
-
-        /*System.out.println(intArr2);
-        System.out.println(Arrays.toString(intArr2));*/
-
-        //int[] arr = {3, 5, -5, 53, 5, 2, 9, 1};
-        /*System.out.println(Arrays.toString(arr));
-        for(int i=0; i<arr.length-1; i++) {
-            for(int j=i; j<arr.length; j++) {
-                if(arr[i] > arr[j]) {
-                    int temp = arr[i];
-                    arr[i] = arr[j];
-                    arr[j] = temp;
-                }
-            }
-        }
-        System.out.println(Arrays.toString(arr));
-        */
-
-        /*String ss = """
-                SELECT * FROM table
-                WHERE ... = ...
-                """;*/
-
-        /*int[] arr = {3, 5, -5, 53, 5, 2, 9, 1};
-        printArr(arr);
-        sortArray(arr);
-        printArr(arr);*/
-
-        /*int i = 10;
-        System.out.println(i);
-        test(i);
-        System.out.println(i);*/
-
-        /*Random rand = new Random();
-        int i = rand.nextInt();
-        int j = rand.nextInt(1000);
-        System.out.println(i + " " + j);
-*/
-        int[] arr = new int[10];
-        randomArrFill(arr);
-        arr[3] = -55325;
-        arr[7] = -44;
-        printArr(arr);
-        arrNegativeToZero(arr);
-        printArr(arr);
-        sortArray(arr);
-        printArr(arr);
+        book.print();
+        test(book);
+        book.print();
 
         /*
-            реализовать и протестировать
-            метод рандомно заполняющий массив
-            метод сортирующий массив
-            метод приравнивает все отрицательные элементы 0
+            class House + конструктор со всеми полями
+            - String address
+            - int floors
+            - int humanCount
+            - public void print() выводит все 3 поля объекта
+
+            в main функции нужно создать объет класса House
+
+            в главном классе нужно разместить 2 функции: addHuman, removeHuman
+            они будут принимать в аргументах объект класса House и увеличивать/уменьшать количество людей на 1
          */
     }
 
-    public static void printArr(int[] arr)
+    public static void test(Book book)
     {
-        String s = "[";
-        for(int i=0; i<arr.length; i++) {
-            s += arr[i];
-            if(i != arr.length-1) {
-                s += ", ";
-            }
-        }
-        s += "]";
-        System.out.println(s);
+        book.pages = 222;
     }
 
-    public static void sortArray(int[] arr)
+    //public static тип_возращемого_значения назнвание_функции(аргументы)
+}
+
+class Book
+{
+    String title;
+    int pages;
+
+    public Book(String title, int pages)
     {
-        for(int i=0; i<arr.length-1; i++) {
-            for(int j=i; j<arr.length; j++) {
-                if(arr[i] > arr[j]) {
-                    int temp = arr[i];
-                    arr[i] = arr[j];
-                    arr[j] = temp;
-                }
-            }
-        }
+        this.title = title;
+        this.pages = pages;
     }
 
-    public static void randomArrFill(int[] arr)
+    public Book(String title)
     {
-        Random rand = new Random();
-        for(int i=0; i<arr.length; i++) {
-            arr[i] = rand.nextInt();
-        }
+        this.title = title;
+        pages = 100;
     }
 
-    public static void arrNegativeToZero(int arr[]) {
-        for(int i=0; i<arr.length; i++) {
-            if(arr[i] < 0) {
-                arr[i] = 0;
-            }
-        }
-    }
-
-    public static void test(int i)
+    public void print()
     {
-        i *= 2;
+        System.out.println("MyBook: " + title + " " + pages);
     }
 }
