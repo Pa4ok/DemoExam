5ffcda223f01e3f2c68b145dc494dc0abfd24eda
Pa4ok <n_leontiev@mail.ru>
2021-09-10 15:30:33 +0300

    10.09.21 массивы в классах но сложнее

--- src/ru/pa4ok/demoexam/App.java
+++ src/ru/pa4ok/demoexam/App.java
@@ -1,6 +1,7 @@
 package ru.pa4ok.demoexam;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 /**
  * shift + F10 - запуск последней конфигурации
@@ -22,11 +23,14 @@ public class App
         Library
         - String address
         - Book[] books
-        - public void addBook(Book book)
+        - public boolean addBook(Book book)
+        - public boolean removeBook(String title, String author)
 
-        при добавлении книги вы ищете первый null элемент в массиве books
-        если такой есть - ставите книгу из аргументов на его место
-        если в массиве нет null элментов, то выводите что нибудь в консоль
+        при попытке доабвления книги вы ищете null элемент в массиве books
+        если такой есть - ставить новую книгу на его месте и возвращаете true, если нет - false
+        при удалении вы ищите не null элемент массива где будут совпадать title и author
+        если такой есть ставите на его место null и возвращаете true, если нет - false
+
      */
 
     public static void main(String[] args)
@@ -35,7 +39,7 @@ public class App
         arr[1] = new Book("1", "1", 10);
         */
 
-        Book[] books = new Book[] {
+        /*Book[] books = new Book[] {
                 null,
                 new Book("1", "1", 10),
                 null
@@ -46,7 +50,22 @@ public class App
         lib.addBook(new Book("2", "2", 10));
         lib.addBook(new Book("3", "3", 10));
         lib.addBook(new Book("4", "4", 10));
-        System.out.println(lib);
+        System.out.println(lib);*/
 
+        /*String s1 = "123321";
+        String s2 = "123";
+
+        Scanner scanner = new Scanner(System.in);
+        s2 += scanner.nextLine();
+
+        System.out.println(s1);
+        System.out.println(s2);
+        System.out.println(s1 == s2);
+        System.out.println(s1.equals(s2));
+        System.out.println(s1.equalsIgnoreCase(s2));*/
+
+        String[] arr = new String[10];
+        System.out.println(Arrays.toString(arr));
+        System.out.println(arr[0].equals("123321"));
     }
 }
