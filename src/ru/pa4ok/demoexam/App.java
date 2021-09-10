727c94a99f6ce4052d0fb0ca81744c958cfcb44a
Pa4ok <n_leontiev@mail.ru>
2021-09-10 16:20:53 +0300

    10.09.21 снова классы и масствы

--- src/ru/pa4ok/demoexam/App.java
+++ src/ru/pa4ok/demoexam/App.java
@@ -1,8 +1,5 @@
 package ru.pa4ok.demoexam;
 
-import java.util.Arrays;
-import java.util.Scanner;
-
 /**
  * shift + F10 - запуск последней конфигурации
  * alt + enter - контекстное меню по исправлению ошибки
@@ -15,41 +12,36 @@ public class App
         все поля приватными + геттеры и сеттеры
         +переопределенный вывод (toString)
 
-        Book
-        - String title
-        - String author
-        - int pages
+            Student
+            - String fio
+            - int age
+            - int level //класс
 
-        Library
-        - String address
-        - Book[] books
-        - public boolean addBook(Book book)
-        - public boolean removeBook(String title, String author)
+            Teacher
+            - String fio
+            - int age
+            - String subject
+            - int exp //стаж
 
-        при попытке доабвления книги вы ищете null элемент в массиве books
-        если такой есть - ставить новую книгу на его месте и возвращаете true, если нет - false
-        при удалении вы ищите не null элемент массива где будут совпадать title и author
-        если такой есть ставите на его место null и возвращаете true, если нет - false
+            School
+            - String address
+            - Teacher[] teachers
+            - Student[] students
+            - public void nextYear()
 
+            метод nextYear обозначет то что в школе прошел 1 год
+            данный метод должен увеличивать возраст всех учителей и студентов на 1
+            также он должен увеличить у них класс и стаж на 1
+
+            если после увеличения класса студента он > 11 то его надо убрать из школы (заменить на null)
+            и вывести информацию в консоль о том что он выпустился
+
+            если после увеличения возраста преподавателя он > 60 то его надо убрать из школы (заменить на null)
+            и вывести информацию в консоль о том что он ушел на пенсию
      */
 
     public static void main(String[] args)
     {
-        Library lib = new Library("dqwdqwdqw", new Book[] {
-                new Book("1", "1", 10),
-                new Book("2", "2", 10),
-                null
-        });
-
-        System.out.println(lib);
-        System.out.println(lib.addBook(new Book("3", "3", 10)));
-        System.out.println(lib.addBook(new Book("4", "4", 10)));
-        System.out.println(lib);
-        System.out.println(lib.removeBook("3", "3"));
-        System.out.println(lib.removeBook("4", "4"));
-        System.out.println(lib.removeBook("1", "1"));
-        System.out.println(lib);
-
 
     }
 }
