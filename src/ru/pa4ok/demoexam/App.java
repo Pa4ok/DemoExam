e7217d489ad3058205c28fb2bb91061eabe687a0
Pa4ok <n_leontiev@mail.ru>
2021-09-21 12:07:12 +0300

    21.09.21 сравнение строк

--- src/ru/pa4ok/demoexam/App.java
+++ src/ru/pa4ok/demoexam/App.java
@@ -1,6 +1,7 @@
 package ru.pa4ok.demoexam;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 /**
  * alt + enter - контекстное меню исправления ошибки
@@ -22,16 +23,21 @@ public class App
         Library
         - String address
         - Book[] books
-        - public void addBook(Book b)
+        - public boolean addBook(Book b)
+        - public boolean removeBook(String title, String author)
 
         при добавлении книги вы ищете первый null элемент в массиве books
         если такой есть - ставите книгу из аргументов на его место
-        если в массиве нет null элментов, то выводите что нибудь в консоль
+        и возвращаете true, если нет - возвращаете false
+
+        при удалении книги вы ищете книгу с таким же названием и автором
+        если такая книга есть - ставите на нее место в массиве null
+        и возвращаете true, если нет - возвращаете false
      */
     
     public static void main(String[] args)
     {
-        Library lib = new Library(
+        /*Library lib = new Library(
                 "we;fjhewoifew",
                 new Book[]{
                         null,
@@ -47,7 +53,23 @@ public class App
         lib.addBook(new Book("book5", "author5", 100));
         lib.addBook(new Book("book6", "author6", 100));
         lib.addBook(new Book("book7", "author7", 100));
-        System.out.println(lib);
+        System.out.println(lib);*/
+
+        String s1 = "123321";
+        String s2 = "123";
+
+        Scanner scanner = new Scanner(System.in);
+        s2 += scanner.nextLine();
+
+        System.out.println(s1 + " " + s1.length());
+        System.out.println(s2 + " " + s2.length()) ;
+        System.out.println(s1 == s2);
+        System.out.println(s1.equals(s2));
+        System.out.println(s1.equalsIgnoreCase(s2));
+
+        /*String[] arr = new String[3];
+        System.out.println(Arrays.toString(arr));
+        System.out.println(arr[0].equals("123"));*/
     }
 }
 
