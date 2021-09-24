343bffdcbda11e6acfb06fefb1d11c89d4603bc3
Pa4ok <n_leontiev@mail.ru>
2021-09-24 10:32:34 +0300

    24.09.21 наследование, расширение задания

--- src/ru/pa4ok/demoexam/App.java
+++ src/ru/pa4ok/demoexam/App.java
@@ -17,13 +17,16 @@ public class App
         - String fio
         - char gender
         - int age
+        - public boolean nextYear() //age++ return true
 
         Student extends Human
         - int level
+        - public boolean nextYear() //вызываем родительскую реализацию, level++, return level > 11
 
         Teacher extends Human
         - String subject
         - int exp
+        - public boolean nextYear() //вызываем родительскую реализацию, exp++, return age > 60
 
         Building
         - String address
@@ -32,21 +35,17 @@ public class App
         School extends Building
         - String title
         - Human[] entities
-        - public void nextYear()
+        - public int nextYearAll()
 
-        метод nextYear() должен перебирать всех обиталей школы и увеличивать им возраст на 1
-        если обитатель является Student увеличить на 1 level
-        если обитатель является Teacher увеличить на 1 exp
-        если level > 11 или age > 60 его необходимо выгнать (заменить на null в массиве)
-        если обитатель не является ни Student, ни Teacher его необходимо сразу заменить на null
-        и вывести информацию в консоль
-
+        метод nextYearAll() должен перебирать всех обиталей школы и вызывать у них метод nextYear()
+        если возврается true текущий объект массива надо заменить на null
+        возвращаемое значение - количество людей покинувших школу на этом этапе
      */
 
 
     public static void main(String[] args)
     {
-        School sc = new School(
+        /*School sc = new School(
                 "oqliwduhqwdud", 5,
                 new Human[] {
                         null,
@@ -68,6 +67,35 @@ public class App
         System.out.println(sc.nextYear());
         System.out.println(sc);
         System.out.println(sc.nextYear());
-        System.out.println(sc);
+        System.out.println(sc);*/
+
+        A[] arr = new A[] {
+                new A(),
+                new A(),
+                new B(),
+                new B()
+        };
+
+        for(A a : arr) {
+            System.out.println(a.getClass());
+            a.test();
+            System.out.println();
+        }
+    }
+}
+
+class A
+{
+    public void test() {
+        System.out.println("test from A");
+    }
+}
+
+class B extends A
+{
+    @Override
+    public void test() {
+        super.test();
+        System.out.println("test from B");
     }
 }
