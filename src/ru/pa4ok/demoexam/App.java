e91167f55fa4f8c010a897d253fd42b9bbc45b39
Pa4ok <n_leontiev@mail.ru>
2021-09-27 17:35:59 +0300

    27.09.21 абстрактные классы

--- src/ru/pa4ok/demoexam/App.java
+++ src/ru/pa4ok/demoexam/App.java
@@ -1,5 +1,7 @@
 package ru.pa4ok.demoexam;
 
+import java.util.Random;
+
 /**
  * shift + F10 - запуск последней конфигурации
  * alt + enter - контекстное меню по исправлению ошибок
@@ -9,33 +11,78 @@ package ru.pa4ok.demoexam;
 public class App
 {
     /*
-        Point
-        - double x
-        - double z
+        abstract Sortinger
+        - public abstract void sort(int[] arr)
+        - public long sortWithTime(int[] arr)
+        сохряняет время, вызывает sort, возвращает потраченное на сортировку время
 
-        Figure
-        - String title
-        - Point[] points
-        - public double calculateSquare() //return -1
+        BubbleSortinger extends Sortinger
+        наследует Sortinger и реализует функцию sort под алгоритм пузырька
 
-        Triangle
-        - конструктор на 3 точки, передает массив из них в родительский конструкор
-        - public double calculateSquare() //считает уже по формуле для триугольника
-
-        Rectangle
-        - конструктор на 4 точки, передает массив из них в родительский конструкор
-        - public double calculateSquare() //считает уже по формуле для прямоугольника
+        создать 2-4 класса с различными алгоритмами сортировки
+        алгоритмы можно взять тут
+        https://proglib.io/p/java-sorting-algorithms
      */
-
 
     public static void main(String[] args)
     {
-        Triangle t1 = new Triangle(
-                new Point(0, 0),
-                new Point(0, 10),
-                new Point(10, 0)
-        );
+        /*Test t = new Test(4);
+        System.out.println(t);*/
 
-        System.out.println(t1.calculateSquare());
+        Test t = new Test(5) {
+            @Override
+            public void test() {
+                //...
+            }
+        };
+
+        long mills = System.currentTimeMillis();
+
+        Random rand = new Random();
+        int i1 = rand.nextInt();
+        int i2 = rand.nextInt(1000);
+    }
+}
+
+abstract class Test
+{
+    public int value;
+
+    public Test(int value) {
+        this.value = value;
+    }
+
+    public abstract void test();
+
+    public void abc() {
+        //...
+        test();
+        //...
+    }
+
+    @Override
+    public String toString() {
+        return "Test{" +
+                "value=" + value +
+                '}';
+    }
+}
+
+abstract class NewTest extends Test
+{
+    public NewTest(int value) {
+        super(value);
+    }
+}
+
+class NewTest1 extends Test
+{
+    public NewTest1(int value) {
+        super(value);
+    }
+
+    @Override
+    public void test() {
+        System.out.println(123);
     }
 }
