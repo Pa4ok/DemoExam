353fdc56d4fb4f7774bccf3170d0b3ec3c2ac927
Pa4ok <n_leontiev@mail.ru>
2021-10-01 16:49:26 +0300

    01.10.21 обработка ошибок, решение задания

--- src/ru/pa4ok/demoexam/App.java
+++ src/ru/pa4ok/demoexam/App.java
@@ -2,9 +2,6 @@ package ru.pa4ok.demoexam;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.util.Random;
 import java.util.Scanner;
 
 /**
@@ -36,84 +33,27 @@ public class App //
         - Book book
      */
 
-    public static void main(String[] args) {
-        //System.out.println("mark1");
+    public static void main(String[] args)
+    {
+        Library lib = new Library(
+                "owqidjopqiwd",
+                new Book[] {
+                        new Book("1", "1", 1),
+                        null
+                }
+        );
 
-        /*try {
-            int[] arr = new int[3];
-            arr[4] = 5;
-        } catch (ArrayIndexOutOfBoundsException e) {
-            System.out.println("ошибка поймана");
-        }*/
-
-        /*try {
-            double d = 5 / 0;
-            int[] arr = new int[3];
-            arr[4] = 5;
-        } catch (ArrayIndexOutOfBoundsException e) {
-            System.out.println("ошибка поймана 1");
-        } catch (ArithmeticException e) {
-            System.out.println("ошибка поймана 2");
-        }*/
-
-        /*try {
-            double d = 5 / 0;
-            int[] arr = new int[3];
-            arr[4] = 5;
-        } catch (ArrayIndexOutOfBoundsException | ArithmeticException e) {
-            System.out.println("ошибка поймана");
-        }*/
-
-        /*try {
-            double d = 5 / 0;
-            int[] arr = new int[3];
-            arr[4] = 5;
-        } catch (Exception e) {
-            System.out.println("ошибка поймана");
-        }*/
-
-        /*try {
-            test();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }*/
-
-        /*try {
-            test2(60);
-        } catch (MyException e) {
-            e.printStackTrace();
-            System.out.println(e.i);
-        }*/
-
-        //System.out.println("mark2");
-
-        Scanner scanner = new Scanner(System.in);
-        String s = scanner.nextLine();
-        int i = Integer.parseInt(s);
-        System.out.println(i * 100);
-    }
-
-    public static void test() throws FileNotFoundException {
-        FileReader fr = new FileReader("my-file.txt");
-    }
-
-    public static void test1(int i) throws MyRuntimeException{
-        if(i > 50) {
-            throw new MyRuntimeException();
-        }
-    }
-
-    public static void test2(int i) throws MyException {
-        if(i > 50) {
-            throw new MyException(433);
-        }
-    }
-
-    public static void test3(int i) {
-        try {
-            FileReader fr = new FileReader("my-file.txt");
-        } catch (Exception e) {
-            throw new RuntimeException("123efwewf", e);
+        while(true)
+        {
+            try {
+                System.out.println();
+                lib.readBook();
+            } catch (BookReadException e) {
+                e.printStackTrace();
+            } catch (LibrarySpaceException e) {
+                System.out.println("В бибилиотеке кончилось место на книге: " + e.getBook());
+                break;
+            }
         }
     }
 }
