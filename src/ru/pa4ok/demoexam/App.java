73b106ce75b74a5fc6702eec15e57e32b1a89174
Pa4ok <n_leontiev@mail.ru>
2021-10-01 14:02:00 +0300

    01.10.21 предыдущий коммит называется неверно, тема - абстрактные классы

--- src/ru/pa4ok/demoexam/App.java
+++ src/ru/pa4ok/demoexam/App.java
@@ -11,7 +11,7 @@ import java.util.Random;
  * alt + insert - контекстное меню генерации кода (конструкторы, toString, геттеры и сеттеры)
  */
 
-public class App
+public class App //
 {
     /*
         abstract Sortinger
