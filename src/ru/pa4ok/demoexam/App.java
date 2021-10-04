f7dcee6d6315f3352fb3cbb6b20961911e4452cd
Pa4ok <n_leontiev@mail.ru>
2021-10-04 12:07:07 +0300

    04.10.21 обработка ошибок, решение задания

--- src/ru/pa4ok/demoexam/App.java
+++ src/ru/pa4ok/demoexam/App.java
@@ -33,79 +33,30 @@ public class App
 
     public static void main(String[] args)
     {
-        System.out.println("mark1");
+        Library lib = new Library(
+                "edfpiewfjoweiffew",
+                new Book[]{
+                        new Book("t1", "a1", 100),
+                        null
+                }
+        );
 
-        /*try {
-            //int[] arr = new int[2];
-            //arr[3] = 5;
-        } catch (ArrayIndexOutOfBoundsException e) {
-            System.out.println("ошибка отловлена " + e.getMessage());
-        }*/
+        System.out.println(lib);
 
-        /*try {
-            //int[] arr = new int[2];
-            //arr[3] = 5;
-            double d = 5 / 0;
-        } catch (ArrayIndexOutOfBoundsException e) {
-            System.out.println("ошибка отловлена " + e.getMessage());
-        } catch (ArithmeticException e) {
-            System.out.println("ошибка отловлена " + e.getMessage());
-        }*/
-
-        /*try {
-            //int[] arr = new int[2];
-            //arr[3] = 5;
-            double d = 5 / 0;
-        } catch (ArrayIndexOutOfBoundsException | ArithmeticException e) {
-            System.out.println("ошибка отловлена " + e.getMessage());
-        }*/
-
-        /*try {
-            //int[] arr = new int[2];
-            //arr[3] = 5;
-            double d = 5 / 0;
-        } catch (Exception e) {
-            System.out.println("ошибка отловлена " + e.getMessage());
-        }*/
-
-        /*try {
-            test();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }*/
-
-        /*try {
-            test2(100);
-        } catch (MyException e) {
-            e.printStackTrace();
-        }*/
-
-        //test3(1000);
-
-        Scanner scanner = new Scanner(System.in);
-        System.out.printf("Введите что-то: ");
-        String s = scanner.nextLine();
-        int i = Integer.parseInt(s);
-
-        System.out.println("mark2");
-    }
-
-    public static void test() throws InterruptedException {
-        Thread.sleep(1000);
-    }
-
-    public static void test2(int i) throws MyException {
-        if(i > 50) {
-            throw new MyException("dlkjweoifjwef");
+        while(true)
+        {
+            try {
+                lib.readBook();
+            } catch (BookReadException e) {
+                e.printStackTrace();
+            } catch (LibrarySpaceException e) {
+                e.printStackTrace();
+                System.out.println("книге " + e.getBook() + " не хватило места") ;
+                break;
+            }
         }
-    }
 
-    public static void test3(int i) {
-        try {
-            test2(i);
-        } catch (MyException e) {
-            throw new RuntimeException("egrgertgrege", e);
-        }
+        System.out.println(lib);
     }
 }
 
