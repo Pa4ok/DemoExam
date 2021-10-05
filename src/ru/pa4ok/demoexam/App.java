640049a5cd8827bd2560e769d7c747a545c8ced9
Pa4ok <n_leontiev@mail.ru>
2021-10-05 17:39:23 +0300

    05.10.21 генерики, колекции

--- src/ru/pa4ok/demoexam/App.java
+++ src/ru/pa4ok/demoexam/App.java
@@ -2,7 +2,9 @@ package ru.pa4ok.demoexam;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Random;
 
 /**
@@ -13,42 +15,108 @@ import java.util.Random;
 
 public class App
 {
-     /*
-        abstract Sortinger
-        - public abstract void sort(int[] arr)
-        - public long sortWithTime(int[] arr)
-        сохряняет время, вызывает sort, возвращает потраченное на сортировку время
+    /*
+        Book
+        - String author
+        - String title
+        - ing pages
 
-        BubbleSortinger extends Sortinger
-        наследует Sortinger и реализует функцию sort под алгоритм пузырька
+        Library
+        - String address
+        - List<Book> books
+        - public Book addBook(String title, String author, int pages)
+        - public Book removeBook(String title, String author, int pages)
+        - public boolean hasBook(String title, String author, int pages)
 
-        создать 2-4 класса с различными алгоритмами сортировки
-        алгоритмы можно взять тут
-        https://proglib.io/p/java-sorting-algorithms
+        методы addBook и removeBook принимают поля книги
+
+        добавить книгу нужно только в том случае, если нет аналогичной
+        если аналогичная книга есть, метод возврает null, если книга успешно добавлена - вернуть ее
+
+        метод удаления книги возвращает объект удаленной из коллекции книги
+        если такой книги в списке нет - возращаете null
      */
-
 
     public static void main(String[] args)
     {
-        int length = 100000;
-        int[] arr1 = new int[length];
-        int[] arr2 = new int[length];
+        /*Container c1 = new Container("123412321");
+        Container c2 = new Container(new Book("wefewfew"));*/
 
-        Random rand = new Random();
-        for(int i=0; i<length; i++) {
-            arr1[i] = arr2[i] = rand.nextInt(10000);
-        }
+        /*Container<String> c1 = new Container<>("ewfwefew");
+        String s = c1.value;
 
-        BubbleSoritinger bubble = new BubbleSoritinger();
-        QuickSortinger quick = new QuickSortinger();
+        Container<Book> c2 = new Container<>(new Book("wefewfew"));
+        Book b = c2.value;*/
 
-        /*System.out.println(Arrays.toString(arr1));
-        System.out.println(Arrays.toString(arr2));*/
+        /*List<String> list = new ArrayList<>();
+        list.add("oqidiqwdqwd");
+        list.add("tyjytj");
+        list.add("jtjy");
+        list.add("ewfd");
+        System.out.println(list);
+        list.remove(1);
+        System.out.println(list);*/
 
-        System.out.println(bubble.sortWithTime(arr1));
-        System.out.println(quick.sortWithTime(arr2));
+        List<String> list = new ArrayList<>(Arrays.asList(
+            "1", "22", "3", "44", "55", "66", "7", "88", "9"
+        ));
 
-        /*System.out.println(Arrays.toString(arr1));
-        System.out.println(Arrays.toString(arr2));*/
+        System.out.println(list);
+
+        /*for(int i=0; i<list.size(); i++) {
+            if(list.get(i).length() > 1) {
+                list.remove(i);
+            }
+        }*/
+
+        /*for(int i=list.size()-1; i>=0; i--) {
+            if(list.get(i).length() > 1) {
+                list.remove(i);
+            }
+        }*/
+
+        //list.removeIf(s -> s.length() > 1);
+
+        /*for(String s : list) {
+        }*/
+        test(list);
+
+        System.out.println(list);
+    }
+
+    public static void test(List<String> list) {
+        list.add("123");
+    }
+}
+
+class Container<T>
+{
+    public T value;
+
+    public Container(T value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return "Container{" +
+                "value=" + value +
+                '}';
+    }
+}
+
+class Book
+{
+    public String title;
+
+    public Book(String title) {
+        this.title = title;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "title='" + title + '\'' +
+                '}';
     }
 }
