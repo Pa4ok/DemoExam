0902fc5b81b7dd93ccd4d2090cf260acfac5f46e
Pa4ok <n_leontiev@mail.ru>
2021-10-06 13:56:39 +0300

    06.10.21 колекции, решение задания

--- src/ru/pa4ok/demoexam/App.java
+++ src/ru/pa4ok/demoexam/App.java
@@ -30,146 +30,23 @@ public class App
 
     public static void main(String[] args)
     {
-        /*MyContainer<String> container1 = new MyContainer<>(
-                new String[]{
-                        "1", "2", "3", "4", "5"
-                }
+        Library lib = new Library(
+                "wpefowepofwe",
+                new ArrayList<>(Arrays.asList(
+                        new Book("t1", "a1", 10),
+                        new Book("t2", "a2", 10),
+                        new Book("t3", "a3", 10),
+                        new Book("t4", "a4", 10)
+                ))
         );
-
-        MyContainer<Book> container2 = new MyContainer<>(
-                new Book[]{
-                        new Book("ewfewffew", "werfewrfew", 10),
-                        new Book("ewfewffew", "werfewrfew", 10),
-                        new Book("ewfewffew", "werfewrfew", 10),
-                        new Book("ewfewffew", "werfewrfew", 10)
-                }
-        );*/
-
-        /*List<String> list = new ArrayList<>();
-        list.add("1");
-        list.add("2");
-        list.add("3");
-        list.add("4");
-        System.out.println(list);
-        list.set(3, "123");
-        System.out.println(list);
-        System.out.println(list.size());
-        System.out.println(list.contains("2"));
-        System.out.println(list.contains("4"));
-        System.out.println(list.remove(2));
-        System.out.println(list.remove("1"));
-        System.out.println(list);*/
-
-        /*List<String> list = new ArrayList<>(Arrays.asList("1", "2", "3"));
-        System.out.println(list);
-        list.add("123");
-        System.out.println(list);
-        test(list);
-        System.out.println(list);
-
-        for(String s : list) {
-            System.out.printf(s + " | ");
-        }
-        System.out.println();
-
-        for(int i=0; i<list.size(); i++) {
-            System.out.printf(list.get(i) + " | ");
-        }
-        System.out.println();*/
-
-        //List<String> list = new ArrayList<>(Arrays.asList("1", "2", "3", "4", "5", "6", "7"));
-        //System.out.println(list);
-
-        //плохо
-        /*for(int i=0; i<list.size(); i++) {
-            if(i % 2 == 1) {
-                list.remove(i);
-            }
-        }*/
-
-        //еще хуже
-        /*for(String s : list) {
-            if(Integer.parseInt(s) % 2 == 0) {
-                list.remove(s);
-            }
-        }*/
-
-        //если уж совсем нужен индекс, можно запустить с конца
-        /*for(int i=list.size()-1; i>=0; i--) {
-            if(i % 2 == 1) {
-                list.remove(i);
-            }
-        }*/
-
-        //удаление с условие всех подходящих элементов
-        //list.removeIf(s -> Integer.parseInt(s) % 2 == 0);
-
-        //System.out.println(list);
-
-        Test t = new Test();
-        System.out.println(t);
-        t.getList().add("123");
-        System.out.println(t);
-    }
-
-    public static void test(List<String> list)
-    {
-        list.set(0, "321");
-    }
-}
-
-class MyContainer<T>
-{
-    private T[] arr;
-
-    public MyContainer(T[] arr) {
-        this.arr = arr;
-    }
-
-    @Override
-    public String toString() {
-        return "MyContainer{" +
-                "arr=" + Arrays.toString(arr) +
-                '}';
-    }
-
-    public T[] getArr() {
-        return arr;
-    }
-
-    public void setArr(T[] arr) {
-        this.arr = arr;
-    }
-}
-
-class Test
-{
-    private List<String> list = new ArrayList<>();
-
-    public Test() {
-    }
-
-    @Override
-    public String toString() {
-        return "Test{" +
-                "list=" + list +
-                '}';
-    }
-
-    public List<String> getList() {
-        return list;
-    }
-}
-
-class Test1
-{
-    private List<String> list;
-
-    public Test1(List<String> list) {
-        this.list = list;
-    }
-
-    public Test1() {
-        this(new ArrayList<>());
+        System.out.println(lib);
+        System.out.println(lib.hasBook("t3", "a3"));
+        System.out.println(lib.hasBook("t5", "a5"));
+        System.out.println(lib.addBook("t5", "a5", 10));
+        System.out.println(lib.addBook("t5", "a5", 10));
+        System.out.println(lib.hasBook("t5", "a5"));
+        System.out.println(lib.removeBook("t1", "a1"));
+        System.out.println(lib.removeBook("t1", "a1"));
+        System.out.println(lib);
     }
 }
