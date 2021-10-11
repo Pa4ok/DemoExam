6da09d997dd3b0e38c36d77372989b0423c3018d
Pa4ok <n_leontiev@mail.ru>
2021-10-11 12:15:34 +0300

    11.10.21 map

--- src/ru/pa4ok/demoexam/App.java
+++ src/ru/pa4ok/demoexam/App.java
@@ -13,29 +13,35 @@ public class App
 {
     /*
         Book
-        - String author
+        - int id
         - String title
+        - String author
         - ing pages
 
         Library
         - String address
-        - List<Set> books
-        - public Book addBook(String title, String author, int pages)
-        - public Book removeBook(String title, String author, int pages)
-        - public boolean hasBook(String title, String author, int pages)
+        - Set<Book> books
+        - Map<Integer, Book> bookIdCache //ключ - id книги, объект - сама книга
 
-        методы addBook и removeBook принимают поля книги
+        - public boolean addBook(Book book)
+        - public boolean hasBook(Book book)
+        - public boolean removeBook(Book book)
 
-        добавить книгу нужно только в том случае, если нет аналогичной
-        если аналогичная книга есть, метод возврает null, если книга успешно добавлена - вернуть ее
+        - public Book addBook(int id, String title, String author, int pages)
+        - public Book hasBook(int bookId)
+        - public Book removeBook(int bookId)
 
-        метод удаления книги возвращает объект удаленной из коллекции книги
-        если такой книги в списке нет - возращаете null
+        при добавлении или удалении книги из библиотеки необходимо обновлять bookIdCache
+
+        методы которые принимают объект возрают true/false если они смогли найти/добавить/удалить такую книгу
+
+        методы которые принимают поля книги/ее id, возврают саму книгу, если они ее нашли/добавали/удалили
+        если не смогли - возрашают null
      */
 
     public static void main(String[] args)
     {
-        Library lib = new Library(
+        /*Library lib = new Library(
                 "grreegerrgger",
                 new HashSet<>(Arrays.asList(
                     new Book("t1", "a1", 10),
@@ -52,7 +58,68 @@ public class App
         System.out.println(lib.hasBook("t4", "a4", 10));
         System.out.println(lib.removeBook("t1", "a1", 10));
         System.out.println(lib.removeBook("t1", "a1", 10));
-        System.out.println(lib);
+        System.out.println(lib);*/
+
+
+
+        Map<String, Test> map = new HashMap<>();
+
+        Test t1 = new Test("test1", 44.54345, 44);
+        Test t2 = new Test("test2", 45.54345, 45);
+        Test t3 = new Test("test3", 46.54345, 46);
+
+        map.put(t1.title, t1);
+        map.put(t2.title, t2);
+        map.put(t3.title, t3);
+
+        /*System.out.println(map);
+        System.out.println(map.get("test1"));
+        System.out.println(map.get("test4"));
+        System.out.println(map.containsKey("test1"));
+        System.out.println(map.containsKey("test4"));
+        System.out.println(map.remove("test1"));
+        System.out.println(map.remove("test4"));
+        System.out.println(map.remove("test2", t3));
+        System.out.println(map);*/
+
+        System.out.println(map);
+        System.out.println();
+
+        Set<String> keys = map.keySet();
+        System.out.println(keys);
+        System.out.println();
+
+        List<Test> values = new ArrayList<>(map.values());
+        System.out.println(values);
+        System.out.println();
+
+        Set<Map.Entry<String, Test>> entries = map.entrySet();
+        for(Map.Entry<String, Test> e : entries) {
+            System.out.println(e.getKey() + " : " + e.getValue());
+        }
+        System.out.println();
+    }
+}
+
+class Test
+{
+    public String title;
+    public double value1;
+    public int value2;
+
+    public Test(String title, double value1, int value2) {
+        this.title = title;
+        this.value1 = value1;
+        this.value2 = value2;
+    }
+
+    @Override
+    public String toString() {
+        return "Test{" +
+                "title='" + title + '\'' +
+                ", value1=" + value1 +
+                ", value2=" + value2 +
+                '}';
     }
 }
 
