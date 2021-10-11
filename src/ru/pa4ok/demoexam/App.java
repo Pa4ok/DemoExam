537c9e0c0a4e912dbf4de4a4adc706544e1293a5
Pa4ok <n_leontiev@mail.ru>
2021-10-11 15:49:39 +0300

    11.10.21 задание с прошлой пары

--- src/ru/pa4ok/demoexam/App.java
+++ src/ru/pa4ok/demoexam/App.java
@@ -17,16 +17,16 @@ public class App
 {
     /*
         Book
-        - String author
         - String title
+        - String author
         - ing pages
 
         Library
         - String address
         - List<Book> books
+        - public boolean hasBook(String title, String author, int pages)
         - public Book addBook(String title, String author, int pages)
         - public Book removeBook(String title, String author, int pages)
-        - public boolean hasBook(String title, String author, int pages)
 
         методы addBook и removeBook принимают поля книги
 
@@ -39,84 +39,21 @@ public class App
 
     public static void main(String[] args)
     {
-        /*Container c1 = new Container("123412321");
-        Container c2 = new Container(new Book("wefewfew"));*/
+        Library lib = new Library(
+                "wefpiewrifgjwe",
+                new ArrayList<>(Arrays.asList(
+                        new Book("title-1", "author-1", 10),
+                        new Book("title-2", "author-2", 10)
+                ))
+        );
 
-        /*Container<String> c1 = new Container<>("ewfwefew");
-        String s = c1.value;
-
-        Container<Book> c2 = new Container<>(new Book("wefewfew"));
-        Book b = c2.value;*/
-
-        /*List<String> list = new ArrayList<>();
-        list.add("oqidiqwdqwd");
-        list.add("tyjytj");
-        list.add("jtjy");
-        list.add("ewfd");
-        System.out.println(list);
-        list.remove(1);
-        System.out.println(list);*/
-
-        List<String> list = new ArrayList<>(Arrays.asList(
-            "1", "22", "3", "44", "55", "66", "7", "88", "9"
-        ));
-
-        System.out.println(list);
-
-        /*for(int i=0; i<list.size(); i++) {
-            if(list.get(i).length() > 1) {
-                list.remove(i);
-            }
-        }*/
-
-        /*for(int i=list.size()-1; i>=0; i--) {
-            if(list.get(i).length() > 1) {
-                list.remove(i);
-            }
-        }*/
-
-        //list.removeIf(s -> s.length() > 1);
-
-        /*for(String s : list) {
-        }*/
-        test(list);
-
-        System.out.println(list);
-    }
-
-    public static void test(List<String> list) {
-        list.add("123");
-    }
-}
-
-class Container<T>
-{
-    public T value;
-
-    public Container(T value) {
-        this.value = value;
-    }
-
-    @Override
-    public String toString() {
-        return "Container{" +
-                "value=" + value +
-                '}';
-    }
-}
-
-class Book
-{
-    public String title;
-
-    public Book(String title) {
-        this.title = title;
-    }
-
-    @Override
-    public String toString() {
-        return "Book{" +
-                "title='" + title + '\'' +
-                '}';
+        System.out.println(lib);
+        System.out.println(lib.hasBook("title-2", "author-2", 10));
+        System.out.println(lib.hasBook("title-3", "author-3", 10));
+        System.out.println(lib.addBook("title-3", "author-3", 10));
+        System.out.println(lib.addBook("title-3", "author-3", 10));
+        System.out.println(lib.removeBook("title-1", "author-1", 10));
+        System.out.println(lib.removeBook("title-1", "author-1", 10));
+        System.out.println(lib);
     }
 }
---
+++ src/ru/pa4ok/demoexam/Library.java
@@ -0,0 +1,67 @@
+package ru.pa4ok.demoexam;
+
+import java.util.List;
+
+public class Library
+{
+    private String address;
+    private List<Book> books;
+
+    public Library(String address, List<Book> books) {
+        this.address = address;
+        this.books = books;
+    }
+
+    public boolean hasBook(String title, String author, int pages) {
+        for(Book b : books) {
+            if(title.equals(b.getTitle()) && author.equals(b.getAuthor()) && pages == b.getPages()) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public Book addBook(String title, String author, int pages) {
+        if(hasBook(title, author, pages)) {
+            return null;
+        }
+        Book b = new Book(title, author, pages);
+        books.add(b);
+        return b;
+    }
+
+    public Book removeBook(String title, String author, int pages) {
+        for(int i=0; i<books.size(); i++) {
+            Book b = books.get(i);
+            if(title.equals(b.getTitle()) && author.equals(b.getAuthor()) && pages == b.getPages()) {
+                books.remove(i);
+                return b;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public String toString() {
+        return "Library{" +
+                "address='" + address + '\'' +
+                ", books=" + books +
+                '}';
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public List<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(List<Book> books) {
+        this.books = books;
+    }
+}
