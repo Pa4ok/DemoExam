4d1fc63919ce721a9634a5cf2af40c4178fe584c
Pa4ok <n_leontiev@mail.ru>
2021-10-12 12:41:00 +0300

    12.10.21 обрабртка ошибок

--- src/ru/pa4ok/demoexam/App.java
+++ src/ru/pa4ok/demoexam/App.java
@@ -1,7 +1,8 @@
 package ru.pa4ok.demoexam;
 
-import java.util.Arrays;
-import java.util.Random;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Scanner;
 
 /**
  * alt + enter - контекстное меню исправления ошибки
@@ -13,50 +14,109 @@ import java.util.Random;
 public class App
 {
     /*
-        abstract Sortinger
-        - public abstract void sort(int[] arr)
-        - public long sortWithTime(int[] arr)
-        сохряняет время, вызывает sort, возвращает потраченное на сортировку время
+        Book
+        - String title (>3 & <20 символов)
+        - String author (>3 & <20 символов)
+        - int pages (>0)
 
-        BubbleSortinger extends Sortinger
-        наследует Sortinger и реализует функцию sort под алгоритм пузырька
+        Library
+        - String address
+        - Book[] books
+        - public void readBook() throws BookReadException, LibrarySpaceException
+            метод должен запрашивать у пользователя все поля книги и создавать из них ее объект после чего добавлять в библиотеку
+            если пользователь вводит неверные значения выбрасывать BookReadException
+            если после чтения полей в бибилиотеке нет места то выбрасывать LibrarySpaceException
 
-        создать 2-4 класса с различными алгоритмами сортировки
-        алгоритмы можно взять тут
-        https://proglib.io/p/java-sorting-algorithms
+        BookReadException extends Exception
+
+        LibrarySpaceException extends Exception
+        - Book book
+
+        создаете библиотеку и в цикле while(true) читаете книги
+        если вылетает BookReadException продолжаете читать
+        если вылетает LibrarySpaceException завершаете цикл
      */
+    public static void main(String[] args) {
+        //System.out.println("mark1");
 
-    public static void main(String[] args)
-    {
-        /*int length = 10000;
-        int[] arr1 = new int[length];
-        int[] arr2 = new int[length];
+        /*try {
+            int[] arr = new int[3];
+            arr[5] = 10;
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("ошибка отловлена " + e.getMessage());
+        }*/
 
-        Random rand = new Random();
-        for(int i=0; i<length; i++) {
-            arr1[i] = arr2[i] = rand.nextInt(10000);
-        }
+        /*try {
+            double d = 5 / 0;
+            int[] arr = new int[3];
+            arr[5] = 10;
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("ошибка отловлена 1 " + e.getMessage());
+        } catch (ArithmeticException e) {
+            System.out.println("ошибка отловлена 2 " + e.getMessage());
+        }*/
 
-        BubbleSortinger bubble = new BubbleSortinger();
-        QuickSortinger quick = new QuickSortinger();
+        /*try {
+            double d = 5 / 0;
+            int[] arr = new int[3];
+            arr[5] = 10;
+        } catch (ArrayIndexOutOfBoundsException | ArithmeticException e) {
+            System.out.println("ошибка отловлена " + e.getMessage());
+        } */
 
-        System.out.println(Arrays.toString(arr1));
-        System.out.println(Arrays.toString(arr2));
-        System.out.println();
+        /*try {
+            double d = 5 / 0;
+            int[] arr = new int[3];
+            arr[5] = 10;
+        } catch (Exception e) {
+            System.out.println("ошибка отловлена " + e.getMessage());
+        }*/
 
-        System.out.println(bubble.sortWithTime(arr1) + "ms");
-        System.out.println(quick.sortWithTime(arr2) + "ms");
-        System.out.println();
+        /*try {
+            FileReader fr = new FileReader("regergreuhgre.txt");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }*/
 
-        System.out.println(Arrays.toString(arr1));
-        System.out.println(Arrays.toString(arr2));*/
+        /*try {
+            test();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }*/
 
-        sort(new BubbleSortinger());
-        sort(new QuickSortinger());
+        /*try {
+            test1(60);
+        } catch (MyException e) {
+            System.out.println("ошибка: " + e.value);
+        }*/
+
+        //test2();
+        //System.out.println("mark2");
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.printf("введите что-то: ");
+        String s = scanner.nextLine();
+        int i = Integer.parseInt(s);
+        System.out.println(i * 100);
     }
 
-    public static void sort(Sortinger sortinger)
-    {
+    public static void test() throws FileNotFoundException {
+        FileReader fr = new FileReader("regergreuhgre.txt");
+    }
+
+    public static void test1(int i) throws MyException {
+        if (i > 50) {
+            throw new MyException(i);
+        }
+    }
+
+    public static void test2() {
+        //...
+        try {
+            FileReader fr = new FileReader("23423432432");
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
         //...
     }
 }
