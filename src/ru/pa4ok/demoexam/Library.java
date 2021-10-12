579b944b5f591381133237f739d76a1733ecaf74
Pa4ok <n_leontiev@mail.ru>
2021-10-12 17:13:16 +0300

    12.10.21 решение задания с прошлой пары

--- src/ru/pa4ok/demoexam/Library.java
+++ src/ru/pa4ok/demoexam/Library.java
@@ -1,44 +1,29 @@
 package ru.pa4ok.demoexam;
 
-import java.util.List;
+import java.util.Set;
 
 public class Library
 {
     private String address;
-    private List<Book> books;
+    private Set<Book> books;
 
-    public Library(String address, List<Book> books) {
+    public Library(String address, Set<Book> books) {
         this.address = address;
         this.books = books;
     }
 
     public boolean hasBook(Book book) {
-        for(Book b : books) {
-            if(b.equals(book)) {
-                return true;
-            }
-        }
-        return false;
+        return books.contains(book);
     }
 
-    /*public Book addBook(String title, String author, int pages) {
-        if(hasBook(title, author, pages)) {
-            return null;
-        }
-        Book b = new Book(title, author, pages);
-        books.add(b);
-        return b;
-    }*/
+    public Book addBook(String title, String author, int pages) {
+        Book book = new Book(title, author, pages);
+        return books.add(book) ? book : null;
+    }
 
     public Book removeBook(String title, String author, int pages) {
-        for(int i=0; i<books.size(); i++) {
-            Book b = books.get(i);
-            if(title.equals(b.getTitle()) && author.equals(b.getAuthor()) && pages == b.getPages()) {
-                books.remove(i);
-                return b;
-            }
-        }
-        return null;
+        Book book = new Book(title, author, pages);
+        return books.remove(book) ? book : null;
     }
 
     @Override
@@ -57,11 +42,11 @@ public class Library
         this.address = address;
     }
 
-    public List<Book> getBooks() {
+    public Set<Book> getBooks() {
         return books;
     }
 
-    public void setBooks(List<Book> books) {
+    public void setBooks(Set<Book> books) {
         this.books = books;
     }
 }
