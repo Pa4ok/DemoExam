a53b4991db07886f5d136e868b529dab9a708d97
Pa4ok <n_leontiev@mail.ru>
2021-10-13 12:28:15 +0300

    13.10.21 set

--- src/ru/pa4ok/demoexam/App.java
+++ src/ru/pa4ok/demoexam/App.java
@@ -1,8 +1,6 @@
 package ru.pa4ok.demoexam;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 /**
  * shift + F10 - запуск выбранной конфигурации (точка входа psvm)
@@ -19,23 +17,15 @@ public class App {
 
         Library
         - String address
-        - List<Book> books
-        - public boolean hasBook(String title, String author, int pages)
-        - public Book addBook(String title, String author, int pages)
-        - public Book removeBook(String title, String author, int pages)
-
-        методы addBook и removeBook принимают поля книги
-
-        добавить книгу нужно только в том случае, если нет аналогичной
-        если аналогичная книга есть, метод возврает null, если книга успешно добавлена - вернуть ее
-
-        метод удаления книги возвращает объект удаленной из коллекции книги
-        если такой книги в списке нет - возращаете null
+        - Set<Book> books
+        - public boolean hasBook(Book book)
+        - public boolean addBook(Book book)
+        - public boolean removeBook(Book book)
      */
 
 
     public static void main(String[] args) {
-        Library lib = new Library(
+        /*Library lib = new Library(
                 "wpeofjpweofjew",
                 new ArrayList<>(Arrays.asList(
                         new Book("title-1", "author-1", 10),
@@ -52,6 +42,71 @@ public class App {
         System.out.println(lib.addBook("title-4", "author-3", 10));
         System.out.println(lib.removeBook("title-1", "author-1", 10));
         System.out.println(lib.removeBook("title-1", "author-1", 10));
-        System.out.println(lib);
+        System.out.println(lib);*/
+
+        /*Book b1 = new Book("title-1", "author-1", 10);
+        Book b2 = new Book("title-1", "author-1", 10);
+
+        System.out.println(b1 == b2);
+        System.out.println(b1.equals(b2));*/
+
+        /*List<Book> list = new ArrayList<>(Arrays.asList(
+                new Book("title-1", "author-1", 10),
+                new Book("title-2", "author-2", 10),
+                new Book("title-3", "author-3", 10)
+        ));
+
+        System.out.println(list);
+        list.remove(new Book("title-2", "author-2", 10));
+        System.out.println(list);
+        System.out.println(list.contains(new Book("title-3", "author-3", 10)));
+        System.out.println(list.contains(new Book("title-4", "author-3", 10)));
+        System.out.println(list.indexOf(new Book("title-3", "author-3", 10)));
+        System.out.println(list.indexOf(new Book("title-4", "author-3", 10)));*/
+
+        /*List<Book> list = new ArrayList<>();
+        for(int i=0; i<1000; i++) {
+            list.add(new Book("title-" + i, "author-" + i, i * 10));
+        }
+
+        long startMills = System.currentTimeMillis();
+
+        for(int i=0; i<1000; i++) {
+            for(int j=0; j<1000; j++) {
+                Book b = new Book("title-" + j, "author-" + j, j * 10);
+                if(!list.contains(b)) {
+                    list.add(b);
+                }
+            }
+        }
+
+        System.out.println((System.currentTimeMillis() - startMills) + "ms");*/
+
+        /*Set<Book> set = new HashSet<>();
+        set.add(new Book("fqwfewef-3", "author-3", 10));
+        set.add(new Book("tiwefweftle-2", "author-2", 10));
+        set.add(new Book("jyttyjtle-1", "author-1", 10));
+        System.out.println(set);
+        set.remove(new Book("tiwefweftle-2", "author-2", 10));
+        System.out.println(set);
+
+        for(Book b : set) {
+            //...
+        }*/
+
+        Set<Book> set = new HashSet<>();
+        for(int i=0; i<1000; i++) {
+            set.add(new Book("title-" + i, "author-" + i, i * 10));
+        }
+
+        long startMills = System.currentTimeMillis();
+
+        for(int i=0; i<1000; i++) {
+            for(int j=0; j<1000; j++) {
+                set.add(new Book("title-" + j, "author-" + j, j * 10));
+            }
+        }
+
+        System.out.println((System.currentTimeMillis() - startMills) + "ms");
     }
 }
