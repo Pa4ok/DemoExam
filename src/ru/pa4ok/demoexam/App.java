69784e842a90600610b17fc5e47066fdd5d53377
Pa4ok <n_leontiev@mail.ru>
2021-10-15 13:54:31 +0300

    15.10.21 set

--- src/ru/pa4ok/demoexam/App.java
+++ src/ru/pa4ok/demoexam/App.java
@@ -1,8 +1,6 @@
 package ru.pa4ok.demoexam;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 /**
  * shift + F10 - запуск последней конфигурации
@@ -21,7 +19,7 @@ public class App //
 
         Library
         - String address
-        - List<Book> books
+        - Set<Book> books
         - public boolean hasBook(Book book)
         - public boolean addBook(Book book)
         - public boolean removeBook(Book book)
@@ -29,113 +27,52 @@ public class App //
 
     public static void main(String[] args)
     {
-        /*Container c1 = new Container("wefwfwefew");
-        Container c2 = new Container(new Book("t1", "a1", 10));
-
-        String s = (String) c1.value;
-        Book b = (Book) c2.value;*/
-
-        /*Container<String> c1 = new Container<>("wefwfwefew");
-        Container<Book> c2 = new Container<>(new Book("t1", "a1", 10));
-
-        String s = c1.value;
-        Book b = c2.value;*/
-
-        /*List<String> list = new ArrayList<>();
-        list.add("one");
-        list.add("two");
-        list.add("three");
-        System.out.println(list);
-        System.out.println(list.get(0));
-        list.set(1, "abc");
-        System.out.println(list);
-        list.remove(0);
-        System.out.println(list);
-        System.out.println(list.size());
-        System.out.println(list.isEmpty());*/
-
-        /*List<String> list = new ArrayList<>(Arrays.asList(
-                "1","22","3","44","55","66","7","88","9"
-        ));*/
-
-        /*for(int i=0; i<list.size(); i++) {
-            System.out.println(list.get(i));
-        }*/
-        /*for(String s : list) {
-            System.out.println(s);
-        }*/
-
-        //System.out.println(list);
-
-        /*for(int i=0; i<list.size(); i++) {
-            if(list.get(i).length() > 1) {
-                list.remove(i);
-                i--;
-            }
-        }*/
-
-        /*for(int i=list.size()-1; i>=0; i--) {
-            if(list.get(i).length() > 1) {
-                list.remove(i);
-            }
-        }*/
-
-        //list.removeIf(s -> s.length() > 1);
-        //System.out.println(list);
-
-        /*Library lib = new Library(
-                "weprigjpqwofqw",
-                new ArrayList<>(Arrays.asList(
-                        new Book("t1", "a1", 10)
-                ))
-        );
-
-        System.out.println(lib);
-        System.out.println(lib.hasBook("t1", "a1", 10));
-        System.out.println(lib.hasBook("t2", "a2", 10));
-        System.out.println(lib.addBook("t2", "a2", 10));
-        System.out.println(lib.addBook("t2", "a2", 10));
-        System.out.println(lib.addBook("t2", "a2", 10));
-        System.out.println(lib.hasBook("t2", "a2", 10));
-        System.out.println(lib.removeBook("t2", "a2", 10));
-        System.out.println(lib.removeBook("t2", "a2", 10));
-        System.out.println(lib);*/
-
-
-        Book b2 = new Book("t2", "a2", 10);
-        List<Book> list = new ArrayList<>(Arrays.asList(
-                new Book("t1", "a1", 10),
-                b2,
-                new Book("t3", "a3", 10)
-        ));
-
-        //System.out.println(list.contains(b2));
-        //System.out.println(list.contains(new Book("t2", "a2", 10)));
-        /*System.out.println(list);
-        System.out.println(list.remove(new Book("t2", "a2", 10)));
-        System.out.println(list);
-
-        */
-
-        for(Book b : list) {
-            System.out.println(b2.equals(b));
+        /*List<Book> list = new ArrayList<>();
+        for(int i=0; i<1000; i++) {
+            list.add(new Book("title-" + i, "author-" + i, i * 10));
         }
 
-    }
-}
+        long mills = System.currentTimeMillis();
 
-class Container<T>
-{
-    public T value;
+        for(int i=0; i<1000; i++) {
+            for(int j=0; j<1000; j++) {
+                Book b = new Book("title-" + j, "author-" + j, j * 10);
+                if(!list.contains(b)) {
+                    list.add(b);
+                }
+            }
+        }
 
-    public Container(T value) {
-        this.value = value;
-    }
+        System.out.println((System.currentTimeMillis() - mills) + "ms");*/
 
-    @Override
-    public String toString() {
-        return "Container{" +
-                "value=" + value +
-                '}';
+        /*Set<Book> set = new HashSet<>();
+        set.add(new Book("1", "1", 1));
+        set.add(new Book("2", "2", 2));
+        set.add(new Book("1", "1", 1));
+        System.out.println(set);
+        System.out.println(set.contains(new Book("2", "2", 2)));
+        System.out.println(set.contains(new Book("3", "2", 2)));
+        System.out.println(set.remove(new Book("2", "2", 2)));
+        System.out.println(set.remove(new Book("3", "2", 2)));
+        System.out.println(set);
+
+        for(Book b : set) {
+            //...
+        }*/
+
+        Set<Book> set = new HashSet<>();
+        for(int i=0; i<1000; i++) {
+            set.add(new Book("title-" + i, "author-" + i, i * 10));
+        }
+
+        long mills = System.currentTimeMillis();
+
+        for(int i=0; i<1000; i++) {
+            for(int j=0; j<1000; j++) {
+                set.add(new Book("title-" + j, "author-" + j, j * 10));
+            }
+        }
+
+        System.out.println((System.currentTimeMillis() - mills) + "ms");
     }
 }
