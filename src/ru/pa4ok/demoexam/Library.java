86f9e7458aa9268548471b2b5b4477593d9da599
Pa4ok <n_leontiev@mail.ru>
2021-10-15 11:29:45 +0300

    15.10.21 map, решение задания

--- src/ru/pa4ok/demoexam/Library.java
+++ src/ru/pa4ok/demoexam/Library.java
@@ -1,30 +1,63 @@
 package ru.pa4ok.demoexam;
 
-import java.util.List;
+import java.util.*;
 
 public class Library
 {
     private String address;
-    private List<Book> books;
+    private Set<Book> books = new HashSet<>();
+    private Map<Integer, Book> bookIdCache = new HashMap<>();
 
-    public Library(String address, List<Book> books) {
+    public Library(String address) {
         this.address = address;
-        this.books = books;
+    }
+
+    public boolean addBook(Book book) {
+        if(!bookIdCache.containsKey(book.getId())) {
+            books.add(book);
+            bookIdCache.put(book.getId(), book);
+            return true;
+        }
+        return false;
     }
 
     public boolean hasBook(Book book)
     {
-        return books.contains(book);
-    }
-
-    public boolean addBook(Book book)
-    {
-        return books.add(book);
+        return bookIdCache.containsKey(book.getId());
     }
 
     public boolean removeBook(Book book)
     {
-        return books.remove(book);
+        if(bookIdCache.remove(book.getId()) != null) {
+            books.remove(book);
+            return true;
+        }
+        return false;
+    }
+
+    public Book addBook(int id, String title, String author, int pages)
+    {
+        if(!bookIdCache.containsKey(id)) {
+            Book book = new Book(id, title, author, pages);
+            books.add(book);
+            bookIdCache.put(id, book);
+            return book;
+        }
+        return null;
+    }
+
+    public Book hasBook(int bookId)
+    {
+        return bookIdCache.get(bookId);
+    }
+
+    public Book removeBook(int bookId)
+    {
+        Book book = bookIdCache.remove(bookId);
+        if(book != null) {
+            books.remove(book);
+        }
+        return book;
     }
 
     @Override
@@ -32,6 +65,7 @@ public class Library
         return "Library{" +
                 "address='" + address + '\'' +
                 ", books=" + books +
+                ", bookIdCache=" + bookIdCache +
                 '}';
     }
 
@@ -43,11 +77,19 @@ public class Library
         this.address = address;
     }
 
-    public List<Book> getBooks() {
+    public Set<Book> getBooks() {
         return books;
     }
 
-    public void setBooks(List<Book> books) {
+    public void setBooks(Set<Book> books) {
         this.books = books;
+    }
+
+    public Map<Integer, Book> getBookIdCache() {
+        return bookIdCache;
+    }
+
+    public void setBookIdCache(Map<Integer, Book> bookIdCache) {
+        this.bookIdCache = bookIdCache;
     }
 }
