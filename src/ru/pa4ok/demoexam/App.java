85ef0337cbacd3160e39d16bfd38569c56b96610
Pa4ok <n_leontiev@mail.ru>
2021-10-15 14:51:41 +0300

    15.10.21 сортировка коллкций

--- src/ru/pa4ok/demoexam/App.java
+++ src/ru/pa4ok/demoexam/App.java
@@ -11,31 +11,77 @@ import java.util.*;
 
 public class App
 {
-    /*
+      /*
         Book
-        - String author
+        - int id
         - String title
+        - String author
         - ing pages
 
         Library
         - String address
         - Set<Book> books
-        - public boolean hasBook(Book book)
-        - public boolean addBook(Book book)
-        - public boolean removeBook(Book book)
+
+        - public List<Book> getSortedById()
+        - public List<Book> getSortedByTitle()
+        - public List<Book> getSortedByPages()
+        - public List<Book> getAuthorBooks(String author)
+
      */
 
     public static void main(String[] args)
     {
-        Library lib = new Library(
-                "wfe;oefwefew",
-                new HashSet<>(Arrays.asList(
-                        new Book("1", "1", 1),
-                        new Book("2", "2", 2),
-                        new Book("3", "3", 3)
-                )));
+        List<Book> list = new ArrayList<>(Arrays.asList(
+                new Book("a", "3", 3),
+                new Book("c", "1", 1),
+                new Book("b", "2", 2)
+        ));
 
-        lib.addBook(new Book("4", "4", 4));
-        lib.getBooks().add(new Book("5", "5", 5));
+        //копия спика
+        //List<Book> copy = new ArrayList<>(list);
+
+        System.out.println(list);
+
+        /*Collections.sort(list, new Comparator<Book>() {
+            @Override
+            public int compare(Book o1, Book o2) {
+                *//*
+                    -1: o1 < o2
+                     0: o1 = o2
+                     1: o1 > o2
+                 *//*
+                if(o1.getPages() > o2.getPages()) {
+                    return 1;
+                }
+                if(o1.getPages() < o2.getPages()) {
+                    return -1;
+                }
+                return 0;
+            }
+        });*/
+
+        /*Collections.sort(list, new Comparator<Book>() {
+            @Override
+            public int compare(Book o1, Book o2) {
+                return Integer.compare(o1.getPages(), o2.getPages());
+            }
+        });*/
+
+        Collections.sort(list, new Comparator<Book>() {
+            @Override
+            public int compare(Book o1, Book o2) {
+                return o1.getTitle().compareTo(o2.getTitle());
+            }
+        });
+
+        //то же самое что и выше
+        //Collections.sort(list, (o1, o2) -> Integer.compare(o1.getPages(), o2.getPages()));
+
+        //снова то же самое что и выше
+        //Collections.sort(list, Comparator.comparingInt(Book::getPages));
+
+        System.out.println(list);
+
+
     }
 }
