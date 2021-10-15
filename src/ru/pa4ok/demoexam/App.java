cf2fe1eb1bcaf910ea10380031a26de64e684ce0
Pa4ok <n_leontiev@mail.ru>
2021-10-15 12:56:19 +0300

    15.10.21 лабораторная работа, fix

--- src/ru/pa4ok/demoexam/App.java
+++ src/ru/pa4ok/demoexam/App.java
@@ -2,6 +2,11 @@ package ru.pa4ok.demoexam;
 
 public class App {
     /*
+        задачи лабораторной работы
+        - создать описанную нижу струтуру
+        - заполнить тестовыми сущностями
+        - продемострировать корректную работу кода
+
         GameServer
         - World world //мир c сущностями
         - int updateCounter //колчество обновлений сервера
@@ -10,9 +15,9 @@ public class App {
         class World
         - List<Entity> entities
         - public void updateWorld()
-        - public List<Entity> getEntitiesInRegion(int x, int y, double range)
+        - public List<Entity> getEntitiesInRegion(int x, int z, double range)
         - public List<Entity> getEntitiesNearEntity(Entity entity, double range)
-        - public List<EntityPlayer> getAllPlayers()
+        - public List<EntityGuard> getGuardiansInRegion(int x, int z, double range)
 
         abstract Entity
         - String title
@@ -42,8 +47,11 @@ public class App {
         потом вызывает у world функцию updateWorld
 
         World
-        функция updateWorld перебирает всех сущностей и вызывает у них функцию update
+        метод updateWorld перебирает всех сущностей и вызывает у них функцию update
         после этого в конце обновления (мира) из списка должны удаляться все сущности у которых health <= 0
+        метод getEntitiesInRegion принимает либо координаты и радиус либо сущность (и берет у нее коррдинаты)
+        и возвращает список сущнсотей в радиусе от этих координат
+        метод getGuardiansInRegion делает то же самое, но ищет только сущностей EntityGuard
 
         Entity описывает вообще всех сущностей
         метод update в нем должен только увеличивать поле age на 1
@@ -60,7 +68,7 @@ public class App {
         метод attackEntityFrom переопределяет родительский метод: если в радиусе 2есть EntityGuard
         то они должны принимать урон вместо игрока (вызывать метод attackEntityFrom для них)
         если рядом нет EntityGuard и после удара игрок выживает он вызывает attackEntityFrom для атакующей сущности
-        функция calculateDamage расчитывает урон игрока по формуле 3 + exp / 2
+        метод calculateDamage расчитывает урон игрока по формуле 3 + exp / 2
         если игрок убивает сущность, то exp увеличивается на 1
 
         EntityMonster наследует Entity
@@ -71,6 +79,14 @@ public class App {
 
         *движение сущностей
         сущность за 1 обновление может смещаться на 1 по xPos и на 1 по zPos
+
+        *убийство
+        когда сущность убивает другую сущность об этом необходимо вывести уведомление в консоль (кто и кого убил)
+
+        *общие требования к коду
+        если класс наследуется другим классом, все его поля должны быть protected, если нет - private
+        у всех классов должен быть переопределен метод toString() и должны быть геттеры и сеттеры
+        если есть необходимость - в классы можно добавлять свои поля и методы
      */
 
     public static void main(String[] args) {
