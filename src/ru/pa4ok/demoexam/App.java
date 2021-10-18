aa914fe19544e3514d4bc609ccd9f0c6fb310895
Pa4ok <n_leontiev@mail.ru>
2021-10-18 15:43:04 +0300

    18.10.21 сортировка коллекций

--- src/ru/pa4ok/demoexam/App.java
+++ src/ru/pa4ok/demoexam/App.java
@@ -1,7 +1,5 @@
 package ru.pa4ok.demoexam;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
 import java.util.*;
 
 /**
@@ -12,61 +10,97 @@ import java.util.*;
 
 public class App
 {
-    /*
+     /*
         Book
         - int id
-        - String author
         - String title
+        - String author
         - ing pages
 
         Library
         - String address
         - Set<Book> books
-        - Map<Integer, Book> bookIdCache //ключ - id книги, объект - сама книга
 
-        - public boolean addBook(Book book)
-        - public boolean hasBook(Book book)
-        - public boolean removeBook(Book book)
-
-        - public Book addBook(int id, String title, String author, int pages)
-        - public Book hasBook(int bookId)
-        - public Book removeBook(int bookId)
-
-        при добавлении или удалении книги из библиотеки необходимо обновлять bookIdCache
-
-        методы которые принимают объект возрают true/false если они смогли найти/добавить/удалить такую книгу
-
-        методы которые принимают поля книги/ее id, возврают саму книгу, если они ее нашли/добавали/удалили
-        если не смогли - возрашают null
+        - public List<Book> getSortedById()
+        - public List<Book> getSortedByTitle()
+        - public List<Book> getSortedByPages()
+        - public List<Book> getAuthorBooks(String author)
      */
 
     public static void main(String[] args)
     {
-        /*Book b1 = new Book("title1", "author1", 10);
-        Book b2 = new Book("title2", "author2", 10);
-        Book b3 = new Book("title3", "author3", 10);
+        List<Test> list = new ArrayList<>(Arrays.asList(
+                new Test(2, "b"),
+                new Test(3, "a"),
+                new Test(1, "c")
+        ));
 
-        Map<String, Book> map = new HashMap<>();
-        map.put(b1.getTitle(), b1);
-        map.put(b2.getTitle(), b2);
-        map.put(b3.getTitle(), b3);
-        System.out.println(map);
+        //копия списка
+        //List<Test> copy = new ArrayList<>(list);
 
-        System.out.println(map.get("title4"));
-        System.out.println(map.get("title2"));
+        System.out.println(list);
 
-        Set<String> keys = map.keySet();
-        System.out.println(keys);
+        /*Collections.sort(list, new Comparator<Test>() {
+            @Override
+            public int compare(Test o1, Test o2) {
+                *//*
+                    -1: o1 < o2
+                     0: o1 = o2
+                     1: o1 > o2
+                 *//*
+                if(o1.value > o2.value) {
+                    return 1;
+                }
+                if(o1.value < o2.value) {
+                    return -1;
+                }
+                return 0;
+            }
+        });*/
 
-        List<Book> values = new ArrayList<>(map.values());
-        System.out.println(values);
+        /*Collections.sort(list, new Comparator<Test>() {
+            @Override
+            public int compare(Test o1, Test o2) {
+                return Integer.compare(o1.value, o2.value);
+            }
+        });*/
 
-        Set<Map.Entry<String, Book>> entries = map.entrySet();
-        for(Map.Entry<String, Book> e : entries) {
-            System.out.println(e.getKey() + " | " + e.getValue());
-        }*/
+        Collections.sort(list, new Comparator<Test>() {
+            @Override
+            public int compare(Test o1, Test o2) {
+                return o1.title.compareTo(o2.title);
+            }
+        });
 
-        List<Integer> list = new ArrayList<>();
-        list.add(3);
-   }
+        //то же самое что и сверху
+        //Collections.sort(list, (o1, o2) -> o1.title.compareTo(o2.title));
+
+        //снова то же самое
+        //Collections.sort(list, Comparator.comparing(o -> o.title));
+
+        System.out.println(list);
+
+        Collections.shuffle(list);
+
+        System.out.println(list);
+    }
+}
+
+class Test
+{
+    int value;
+    String title;
+
+    public Test(int value, String title) {
+        this.value = value;
+        this.title = title;
+    }
+
+    @Override
+    public String toString() {
+        return "Test{" +
+                "value=" + value +
+                ", title='" + title + '\'' +
+                '}';
+    }
 }
