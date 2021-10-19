2cdcbbaca861963a7fd7ced30f1f091b99f43d2f
Pa4ok <n_leontiev@mail.ru>
2021-10-19 16:15:26 +0300

    19.10.21 списки, сравнение объектов

--- src/ru/pa4ok/demoexam/App.java
+++ src/ru/pa4ok/demoexam/App.java
@@ -2,6 +2,7 @@ package ru.pa4ok.demoexam;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 /**
  * alt + enter - контекстное меню исправления ошибки
@@ -21,9 +22,9 @@ public class App
         Library
         - String address
         - List<Book> books
-        - public boolean hasBook(String title, String author, int pages)
-        - public Book addBook(String title, String author, int pages)
-        - public Book removeBook(String title, String author, int pages)
+        - public boolean hasBook(Book book)
+        - public Book addBook(Book book)
+        - public Book removeBook(Book book)
 
         методы addBook и removeBook принимают поля книги
         добавить книгу нужно только в том случае, если нет аналогичной
@@ -34,7 +35,7 @@ public class App
 
     public static void main(String[] args)
     {
-        Library lib = new Library(
+        /*Library lib = new Library(
                 "we;lfjweopfgwef",
                 new ArrayList<>(Arrays.asList(
                         new Book("t1", "a1", 10),
@@ -45,7 +46,32 @@ public class App
 
         System.out.println(lib);
         lib.addBook("12312e", "321423432", 44);
-        System.out.println(lib);
+        System.out.println(lib);*/
+
+        /*Book b1 = new Book("t1", "a1", 10);
+        Book b2 = new Book("t1", "a1", 10);
+        System.out.println(b1 == b2);
+        System.out.println(b1.equals(b2));
+        */
+
+        List<Book> list = new ArrayList<>(Arrays.asList(
+                new Book("t1", "a1", 10),
+                new Book("t2", "a2", 10),
+                new Book("t3", "a3", 10)
+        ));
+
+        System.out.println(list);
+
+        System.out.println(list.contains(new Book("t2", "a2", 10)));
+        System.out.println(list.indexOf(new Book("t2", "a2", 10)));
+
+        System.out.println(list.remove(new Book("t2", "a2", 10)));
+
+        System.out.println(list.contains(new Book("t2", "a2", 10)));
+        System.out.println(list.indexOf(new Book("t2", "a2", 10)));
+
+        System.out.println(list);
+
     }
 }
 
--- src/ru/pa4ok/demoexam/Book.java
+++ src/ru/pa4ok/demoexam/Book.java
@@ -1,5 +1,7 @@
 package ru.pa4ok.demoexam;
 
+import java.util.Objects;
+
 public class Book
 {
     private String title;
@@ -10,6 +12,19 @@ public class Book
         this.title = title;
         this.author = author;
         this.pages = pages;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return pages == book.pages && Objects.equals(title, book.title) && Objects.equals(author, book.author);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(title, author, pages);
     }
 
     @Override
