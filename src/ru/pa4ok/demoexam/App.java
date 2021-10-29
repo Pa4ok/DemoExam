8aff0127767a11a6bcea1d2ee46056f7dad5df45
Pa4ok <n_leontiev@mail.ru>
2021-10-29 15:50:58 +0300

    29.10.21 базы данных

--- src/ru/pa4ok/demoexam/App.java
+++ src/ru/pa4ok/demoexam/App.java
@@ -6,6 +6,7 @@ import ru.pa4ok.demoexam.manager.BookEntityManager;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.*;
 
 /**
  * shift + F10 - запуск последней конфигурации
@@ -32,22 +33,41 @@ public class App
          - String fio
          - int yearOfBirth
          - String profession
-         создать таблицу в базе, сущность в программе и метод добавления сущностей в базу
+         создать таблицу в базе, сущность в программе и все 5 методов взаимодействия 
      */
 
     public static void main(String[] args)
     {
-        BookEntity book = new BookEntity(229, "t-e0fpjew", "ayhew9gfew", 222);
+        //BookEntity book = new BookEntity(229, "t-e0fpjew", "ayhew9gfew", 222);
 
-        System.out.println(book);
+        //System.out.println(book);
 
         try {
-            BookEntityManager.insert(book);
+            //BookEntityManager.insert(book);
+
+            /*BookEntity b3 = BookEntityManager.selectById(3);
+            BookEntity b10 = BookEntityManager.selectById(10);
+            System.out.println(b3);
+            System.out.println(b10);*/
+
+            /*List<BookEntity> list = BookEntityManager.selectAll();
+            for(BookEntity book : list) {
+                System.out.println(book);
+            }*/
+
+            /*BookEntity b2 = BookEntityManager.selectById(2);
+            b2.setTitle("b2 title");
+            b2.setAuthor("Pa4ok");
+            b2.setPages(228);
+            BookEntityManager.update(b2);*/
+
+            BookEntityManager.delete(4);
+
         } catch (SQLException e) {
             e.printStackTrace();
         }
 
-        System.out.println(book);
+        //System.out.println(book);
     }
 
     public static Connection getConnection() throws SQLException {
