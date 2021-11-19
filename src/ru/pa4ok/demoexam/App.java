8a8af94f83e27c7f4326059b4a71b931cc5c8f18
Pa4ok <n_leontiev@mail.ru>
2021-11-19 14:24:02 +0300

    19.11.21 десктопная разработка

--- src/ru/pa4ok/demoexam/App.java
+++ src/ru/pa4ok/demoexam/App.java
@@ -1,12 +1,11 @@
 package ru.pa4ok.demoexam;
 
-import ru.pa4ok.demoexam.entity.BookEntity;
-import ru.pa4ok.demoexam.manager.BookEntityManager;
+import ru.pa4ok.demoexam.ui.CreateBookForm;
 
+import javax.swing.*;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.*;
 
 /**
  * shift + F10 - запуск последней конфигурации
@@ -28,49 +27,23 @@ import java.util.*;
 public class App
 {
     /*
-         UserEntity
-         - int id (автоинкремент)
-         - String fio
-         - int yearOfBirth
-         - String profession
-         создать таблицу в базе, сущность в программе и все 5 методов взаимодействия 
+        берем первую базу, которую чинили (database-2)
+        и делаем форму для добавления сущностей в таблицу Product
+        починить таблицу -> сделать класс сущность и менеджер -> сделать форму
      */
 
     public static void main(String[] args)
     {
-        //BookEntity book = new BookEntity(229, "t-e0fpjew", "ayhew9gfew", 222);
-
-        //System.out.println(book);
-
         try {
-            //BookEntityManager.insert(book);
-
-            /*BookEntity b3 = BookEntityManager.selectById(3);
-            BookEntity b10 = BookEntityManager.selectById(10);
-            System.out.println(b3);
-            System.out.println(b10);*/
-
-            /*List<BookEntity> list = BookEntityManager.selectAll();
-            for(BookEntity book : list) {
-                System.out.println(book);
-            }*/
-
-            /*BookEntity b2 = BookEntityManager.selectById(2);
-            b2.setTitle("b2 title");
-            b2.setAuthor("Pa4ok");
-            b2.setPages(228);
-            BookEntityManager.update(b2);*/
-
-            BookEntityManager.delete(4);
-
-        } catch (SQLException e) {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception e) {
             e.printStackTrace();
         }
 
-        //System.out.println(book);
+        new CreateBookForm();
     }
 
     public static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection("jdbc:mysql://localhost:3306/demoexam", "root", "1234");
+        return DriverManager.getConnection("jdbc:mysql://116.202.236.174:3306/DemoExam", "DemoExam", "DemoExam");
     }
 }
---
+++ src/ru/pa4ok/demoexam/ui/CreateBookForm.java
@@ -0,0 +1,75 @@
+package ru.pa4ok.demoexam.ui;
+
+import ru.pa4ok.demoexam.entity.BookEntity;
+import ru.pa4ok.demoexam.manager.BookEntityManager;
+import ru.pa4ok.demoexam.util.BaseForm;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.SQLException;
+
+public class CreateBookForm extends BaseForm
+{
+    private JPanel mainPanel;
+    private JTextField titleField;
+    private JTextField authorField;
+    private JButton saveButton;
+    private JSpinner pageSpinner;
+
+    public CreateBookForm()
+    {
+        super("My super app", 450, 250);
+        setContentPane(mainPanel);
+
+        /*
+        //вариант 1
+        saveButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //...
+            }
+        });
+
+        //вариант 2
+        saveButton.addActionListener(e -> {
+            //...
+        });*/
+
+        saveButton.addActionListener(e ->
+        {
+            String title = titleField.getText().trim();
+            if(title.isEmpty() || title.length() > 256) {
+                System.out.println("Слишком короткое или длинное название");
+                return;
+            }
+
+            String author = authorField.getText().trim();
+            if(author.isEmpty() || author.length() > 256) {
+                System.out.println("Слишком длинный или короткий автор");
+                return;
+            }
+
+            int pages = (int) pageSpinner.getValue();
+            if(pages <= 0) {
+                System.out.println("Количество страниц введено неверно");
+                return;
+            }
+
+            BookEntity book = new BookEntity(title, author, pages);
+
+            try {
+                BookEntityManager.insert(book);
+            } catch (SQLException ex) {
+                System.out.println("Ошибка сохранения данных: " + ex.getMessage());
+                ex.printStackTrace();
+                return;
+            }
+
+            System.out.println("Книга успешно добавлена: " + book);
+        });
+
+        setVisible(true);
+    }
+}
