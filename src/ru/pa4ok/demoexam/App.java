b26aa7e1267451fec8082acf2624b4586d0d1619
Pa4ok <n_leontiev@mail.ru>
2021-11-19 10:40:41 +0300

    19.11.21 десктопная разработка

--- src/ru/pa4ok/demoexam/App.java
+++ src/ru/pa4ok/demoexam/App.java
@@ -1,17 +1,11 @@
 package ru.pa4ok.demoexam;
 
-import ru.pa4ok.demoexam.entity.BookEntity;
-import ru.pa4ok.demoexam.entity.BookEntityCopy;
-import ru.pa4ok.demoexam.manager.BookEntityManager;
+import ru.pa4ok.demoexam.ui.BookCreateForm;
 
+import javax.swing.*;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.List;
-import java.util.concurrent.TimeUnit;
 
 /**
  * shift + F10 - запуск выбранной конфигурации (точка входа psvm)
@@ -35,64 +29,20 @@ import java.util.concurrent.TimeUnit;
 public class App
 {
     /*
-        UserEntity
-        - int id (автоинкремент)
-        - String fio
-        - Date birthday
-        - String profession
-
-         создать таблицу в базе, сущность в программе и менеджер на все 5 базовых методов работы с бд
+        берем первую базу, которую чинили (database-1)
+        и делаем форму для добавления сущностей в таблицу Client
+        починить таблицу -> сделать класс сущность и менеджер -> сделать форму
      */
 
     public static void main(String[] args)
     {
-        /*long mills = System.currentTimeMillis();
-        System.out.println(mills);
-
-        Date date = new Date();
-        System.out.println(date);
-
-        Date date1 = new Date(0);
-        System.out.println(date1);*/
-
-        /*Date date = new Date();
-        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
-        System.out.println(format.format(date));
-        */
-
-        /*String s = "07.28.20";
-        SimpleDateFormat format = new SimpleDateFormat("MM.dd.yy");
         try {
-            Date date = format.parse(s);
-            System.out.println(date);
-
-        } catch (ParseException e) {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception e) {
             e.printStackTrace();
-        }*/
+        }
 
-        //System.out.println(System.currentTimeMillis());
-        //System.out.println(new Date().getTime());
-
-        /*Date date1 = new Date(0);
-        Date date2 = new Date();
-
-        System.out.println(date1.compareTo(date2));
-        System.out.println(date1.before(date2));*/
-
-        //Date date = new Date(TimeUnit.DAYS.toMillis(14));
-        //System.out.println(date);
-
-        /*try {
-            //BookEntityManager.insert(new BookEntity("123", "312", 222, new Date(TimeUnit.DAYS.toMillis(10000))));
-
-            System.out.println(BookEntityManager.selectAll());
-
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }*/
-
-        BookEntityCopy book = new BookEntityCopy("123", "312", 222, new Date(TimeUnit.DAYS.toMillis(10000)));
-        System.out.println(book);
+        new BookCreateForm();
     }
 
     public static Connection getConnection() throws SQLException {
--- src/ru/pa4ok/demoexam/entity/BookEntity.java
+++ src/ru/pa4ok/demoexam/entity/BookEntity.java
@@ -1,8 +1,13 @@
 package ru.pa4ok.demoexam.entity;
 
+import lombok.*;
+
 import java.util.Date;
 import java.util.Objects;
 
+@NoArgsConstructor
+@AllArgsConstructor
+@Data //@EqualsAndHashCode @ToString @Getter @Setter
 public class BookEntity
 {
     private int id;
@@ -11,84 +16,7 @@ public class BookEntity
     private int pages;
     private Date writeDateTime;
 
-    public BookEntity(int id, String title, String author, int pages, Date writeDateTime) {
-        this.id = id;
-        this.title = title;
-        this.author = author;
-        this.pages = pages;
-        this.writeDateTime = writeDateTime;
-    }
-
     public BookEntity(String title, String author, int pages, Date writeDateTime) {
         this(-1, title, author, pages, writeDateTime);
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        BookEntity book = (BookEntity) o;
-        return id == book.id && pages == book.pages && Objects.equals(title, book.title) && Objects.equals(author, book.author) && Objects.equals(writeDateTime, book.writeDateTime);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, title, author, pages, writeDateTime);
-    }
-
-    @Override
-    public String toString() {
-        return "BookEntity{" +
-                "id=" + id +
-                ", title='" + title + '\'' +
-                ", author='" + author + '\'' +
-                ", pages=" + pages +
-                ", writeDateTime=" + writeDateTime +
-                '}';
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public BookEntity setId(int id) {
-        this.id = id;
-        return this;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public BookEntity setTitle(String title) {
-        this.title = title;
-        return this;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
-
-    public BookEntity setAuthor(String author) {
-        this.author = author;
-        return this;
-    }
-
-    public int getPages() {
-        return pages;
-    }
-
-    public BookEntity setPages(int pages) {
-        this.pages = pages;
-        return this;
-    }
-
-    public Date getWriteDateTime() {
-        return writeDateTime;
-    }
-
-    public BookEntity setWriteDateTime(Date writeDateTime) {
-        this.writeDateTime = writeDateTime;
-        return this;
     }
 }
