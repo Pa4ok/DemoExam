ce7d5adc87a736f7a2e506a06ad851fb017d65db
Pa4ok <n_leontiev@mail.ru>
2021-11-30 14:10:49 +0300

    30.11.21 работа с таблицами

--- src/ru/pa4ok/demoexam/ui/BookTableForm.java
+++ src/ru/pa4ok/demoexam/ui/BookTableForm.java
@@ -4,19 +4,31 @@ import ru.pa4ok.demoexam.entity.BookEntity;
 import ru.pa4ok.demoexam.manager.BookEntityManager;
 import ru.pa4ok.demoexam.util.BaseForm;
 import ru.pa4ok.demoexam.util.CustomTableModel;
+import ru.pa4ok.demoexam.util.DialogUtil;
 
 import javax.swing.*;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.sql.SQLException;
+import java.util.*;
 
 public class BookTableForm extends BaseForm
 {
     private JPanel mainPanel;
     private JTable table;
     private JButton addButton;
+    private JComboBox<String> authorFilterBox;
+    private JComboBox pageFilterBox;
+    private JButton idSortButton;
+    private JButton dateSortButton;
+    private JButton clearButton;
 
     private CustomTableModel<BookEntity> model;
+
+    private boolean idSort = true;
+    private boolean dateSort = false;
 
     public BookTableForm()
     {
@@ -24,6 +36,7 @@ public class BookTableForm extends BaseForm
         setContentPane(mainPanel);
 
         initTable();
+        initBoxes();
         initButtons();
 
         setVisible(true);
@@ -43,6 +56,7 @@ public class BookTableForm extends BaseForm
 
         } catch (SQLException e) {
             e.printStackTrace();
+            DialogUtil.showError(this, "Ошибка работы с бд");
         }
 
         table.addMouseListener(new MouseAdapter() {
@@ -57,11 +71,118 @@ public class BookTableForm extends BaseForm
         });
     }
 
+    private void initBoxes()
+    {
+        authorFilterBox.addItem("Все авторы");
+        try {
+            List<BookEntity> list = BookEntityManager.selectAll();
+            Set<String> authors = new HashSet<>();
+            for(BookEntity b : list) {
+                authors.add(b.getAuthor());
+            }
+            for(String s : authors) {
+                authorFilterBox.addItem(s);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            DialogUtil.showError(this, "Ошибка работы с бд");
+        }
+
+        authorFilterBox.addItemListener(new ItemListener() {
+            @Override
+            public void itemStateChanged(ItemEvent e) {
+                if(e.getStateChange() == ItemEvent.SELECTED) {
+                    applyFilters();
+                }
+            }
+        });
+
+        pageFilterBox.addItem("Все");
+        pageFilterBox.addItem(10);
+        pageFilterBox.addItem(100);
+        pageFilterBox.addItem(1000);
+
+        pageFilterBox.addItemListener(new ItemListener() {
+            @Override
+            public void itemStateChanged(ItemEvent e) {
+                if(e.getStateChange() == ItemEvent.SELECTED) {
+                    applyFilters();
+                }
+            }
+        });
+    }
+
+    private void applyFilters()
+    {
+        try {
+            List<BookEntity> allBooks = BookEntityManager.selectAll();
+
+            if(authorFilterBox.getSelectedIndex() != 0) {
+                allBooks.removeIf(b -> !b.getAuthor().equals(authorFilterBox.getSelectedItem()));
+            }
+
+            if(pageFilterBox.getSelectedIndex() != 0) {
+                allBooks.removeIf(b -> b.getPages() < (int)pageFilterBox.getSelectedItem());
+            }
+
+            idSort = true;
+            dateSort = false;
+
+            model.setRows(allBooks);
+            model.fireTableDataChanged();
+
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+            DialogUtil.showError(BookTableForm.this, "Ошибка работы с бд");
+        }
+    }
+
     private void initButtons()
     {
         addButton.addActionListener(e -> {
             dispose();
             new BookCreateForm();
+        });
+
+        idSortButton.addActionListener(e -> {
+            Collections.sort(model.getRows(), new Comparator<BookEntity>() {
+                @Override
+                public int compare(BookEntity o1, BookEntity o2) {
+                    if(idSort) {
+                        return Integer.compare(o2.getId(), o1.getId());
+                    } else {
+                        return Integer.compare(o1.getId(), o2.getId());
+                    }
+                }
+            });
+
+            idSort = !idSort;
+            dateSort = false;
+
+            model.fireTableDataChanged();
+        });
+
+        dateSortButton.addActionListener(e -> {
+            Collections.sort(model.getRows(), new Comparator<BookEntity>() {
+                @Override
+                public int compare(BookEntity o1, BookEntity o2) {
+                    if(dateSort) {
+                        return o2.getWriteDateTime().compareTo(o1.getWriteDateTime());
+                    } else {
+                        return o1.getWriteDateTime().compareTo(o2.getWriteDateTime());
+                    }
+                }
+            });
+
+            dateSort = !dateSort;
+            idSort = false;
+
+            model.fireTableDataChanged();
+        });
+
+        clearButton.addActionListener(e -> {
+            authorFilterBox.setSelectedIndex(0);
+            pageFilterBox.setSelectedIndex(0);
         });
     }
 }
