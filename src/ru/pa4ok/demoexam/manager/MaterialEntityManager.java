fe6db1662c5bf58f37836b38f9e7a80736450108
Erthela <Eldar2358>
2022-01-26 14:37:12 +0300

    26.01.21 разбор варианта с декабря

--- src/ru/pa4ok/demoexam/manager/MaterialEntityManager.java
+++ src/ru/pa4ok/demoexam/manager/MaterialEntityManager.java
@@ -3,15 +3,63 @@ package ru.pa4ok.demoexam.manager;
 import ru.pa4ok.demoexam.Application;
 import ru.pa4ok.demoexam.entity.MaterialEntity;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
 public class MaterialEntityManager
 {
+    public static void insert(MaterialEntity material) throws SQLException
+    {
+        try(Connection c = Application.getConnection())
+        {
+            String sql = "INSERT INTO Material(Title, MaterialType, Unit, Cost, CountInPack, CountInStock, MinCount, Description, Image) VALUES(?,?,?,?,?,?,?,?,?)";
+
+            PreparedStatement ps = c.prepareStatement(sql, PreparedStatement.RETURN_GENERATED_KEYS);
+            ps.setString(1, material.getTitle());
+            ps.setString(2, material.getMaterialType());
+            ps.setString(3, material.getUnit());
+            ps.setDouble(4, material.getCost());
+            ps.setInt(5, material.getPackCount());
+            ps.setInt(6, material.getStockCount());
+            ps.setInt(7, material.getMinCount());
+            ps.setString(8, material.getDesc());
+            ps.setString(9, material.getImagePath());
+
+            ps.executeUpdate();
+
+            ResultSet keys = ps.getGeneratedKeys();
+            if(keys.next()) {
+                material.setId(keys.getInt(1));
+                return;
+            }
+
+            throw new SQLException("entity not added");
+        }
+    }
+
+    public static void update(MaterialEntity material) throws SQLException
+    {
+        try(Connection c = Application.getConnection())
+        {
+            String sql = "UPDATE Material SET Title=?, MaterialType=?, Unit=?, Cost=?, CountInPack=?, CountInStock=?, MinCount=?, Description=?, Image=? WHERE ID=?";
+
+            PreparedStatement ps = c.prepareStatement(sql);
+            ps.setString(1, material.getTitle());
+            ps.setString(2, material.getMaterialType());
+            ps.setString(3, material.getUnit());
+            ps.setDouble(4, material.getCost());
+            ps.setInt(5, material.getPackCount());
+            ps.setInt(6, material.getStockCount());
+            ps.setInt(7, material.getMinCount());
+            ps.setString(8, material.getDesc());
+            ps.setString(9, material.getImagePath());
+            ps.setInt(10, material.getId());
+
+            ps.executeUpdate();
+        }
+    }
+
     public static List<MaterialEntity> selectAll() throws SQLException
     {
         try(Connection c = Application.getConnection())
@@ -37,6 +85,17 @@ public class MaterialEntityManager
             }
 
             return list;
+        }
+    }
+
+    public static void delete(MaterialEntity material) throws SQLException
+    {
+        try(Connection c = Application.getConnection())
+        {
+            String sql = "DELETE FROM Material WHERE ID=?";
+            PreparedStatement ps = c.prepareStatement(sql);
+            ps.setInt(1, material.getId());
+            ps.executeUpdate();
         }
     }
 }
--- src/ru/pa4ok/demoexam/ui/MaterialTableForm.java
+++ src/ru/pa4ok/demoexam/ui/MaterialTableForm.java
@@ -8,12 +8,26 @@ import ru.pa4ok.demoexam.util.DialogUtil;
 
 import javax.swing.*;
 import javax.swing.table.TableColumn;
+import java.awt.event.ItemEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.sql.SQLException;
+import java.util.*;
 
 public class MaterialTableForm extends BaseForm
 {
     private JPanel mainPanel;
     private JTable table;
+    private JButton addButton;
+    private JButton costSortButton;
+    private JComboBox typeFilterBox;
+    private JComboBox costFilterBox;
+    private JButton clearButton;
+    private JLabel rowCountLabel;
+    private JButton helpButton;
+    private JButton dealButton;
+
+    private boolean costSort;
 
     private CustomTableModel<MaterialEntity> model;
 
@@ -23,6 +37,8 @@ public class MaterialTableForm extends BaseForm
         setContentPane(mainPanel);
 
         initTable();
+        initBoxes();
+        initButtons();
 
         setVisible(true);
     }
@@ -49,9 +65,142 @@ public class MaterialTableForm extends BaseForm
             column.setPreferredWidth(0);
             column.setMaxWidth(0);
 
+            updateRowCountLabel(model.getRowCount(), model.getRowCount());
+            if(model.getRowCount() == 0) {
+                DialogUtil.showInfo(this, "В бд не найдено ни 1 записи");
+            }
+
         } catch (SQLException e) {
             e.printStackTrace();
             DialogUtil.showError(this, "Ошибка получения данных: " + e.getMessage());
         }
+
+        table.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                if(e.getClickCount() == 2) {
+                    int row = table.rowAtPoint(e.getPoint());
+                    if(row != -1) {
+                        dispose();
+                        new MaterialEditForm(model.getRows().get(row));
+                    }
+                }
+            }
+        });
+    }
+
+    private void initBoxes()
+    {
+        try {
+            List<MaterialEntity> list = MaterialEntityManager.selectAll();
+            Set<String> types = new HashSet<>();
+            for(MaterialEntity m : list) {
+                types.add(m.getMaterialType());
+            }
+            typeFilterBox.addItem("Все типы");
+            for(String s : types) {
+                typeFilterBox.addItem(s);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            DialogUtil.showError(this, "Ошибка получения данных");
+        }
+
+        typeFilterBox.addItemListener(e -> {
+            if(e.getStateChange() == ItemEvent.SELECTED) {
+                applyFilters();
+            }
+        });
+
+        costFilterBox.addItemListener(e -> {
+            if(e.getStateChange() == ItemEvent.SELECTED) {
+                applyFilters();
+            }
+        });
+    }
+
+    private void applyFilters()
+    {
+        try {
+            List<MaterialEntity> all = MaterialEntityManager.selectAll();
+            int max = all.size();
+
+            if(typeFilterBox.getSelectedIndex() != 0) {
+                all.removeIf(material -> !material.getMaterialType().equals(typeFilterBox.getSelectedItem()));
+            }
+
+            switch (costFilterBox.getSelectedIndex())
+            {
+                case 1:
+                    all.removeIf(material -> material.getCost() >= 5000);
+                    break;
+                case 2:
+                    all.removeIf(material -> material.getCost() < 5000 || material.getCost() >= 15000);
+                    break;
+                case 3:
+                    all.removeIf(material -> material.getCost() < 15000 || material.getCost() >= 50000);
+                    break;
+                case 4:
+                    all.removeIf(material -> material.getCost() < 50000 || material.getCost() >= 100000);
+                    break;
+                case 5:
+                    all.removeIf(material -> material.getCost() < 100000);
+                    break;
+            }
+
+            model.setRows(all);
+            model.fireTableDataChanged();
+
+            updateRowCountLabel(all.size(), max);
+            costSort = false;
+            if(all.isEmpty()) {
+                DialogUtil.showInfo(this, "В бд найдено ни 1 записи после применения фильтров");
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            DialogUtil.showError(this, "Ошибка получения данных");
+        }
+    }
+
+    private void initButtons()
+    {
+        addButton.addActionListener(e -> {
+            dispose();
+            new MaterialCreateForm();
+        });
+
+        costSortButton.addActionListener(e -> {
+            Collections.sort(model.getRows(), new Comparator<MaterialEntity>() {
+                @Override
+                public int compare(MaterialEntity o1, MaterialEntity o2) {
+                    if(costSort) {
+                        return Double.compare(o2.getCost(), o1.getCost());
+                    } else {
+                        return Double.compare(o1.getCost(), o2.getCost());
+                    }
+                }
+            });
+            costSort = !costSort;
+            model.fireTableDataChanged();
+        });
+
+        clearButton.addActionListener(e -> {
+            typeFilterBox.setSelectedIndex(0);
+            costFilterBox.setSelectedIndex(0);
+        });
+
+        helpButton.addActionListener(e -> {
+            DialogUtil.showInfo(this, "Редактирование - двойной клик по запили\nУдаление - внутри формы редактирования");
+        });
+
+        dealButton.addActionListener(e -> {
+            DialogUtil.showInfo(this, "Связь с разработчиком: vasya_2008@mail.ru");
+        });
+    }
+
+    private void updateRowCountLabel(int actual, int max)
+    {
+        rowCountLabel.setText("Записей: " + actual + " / " + max);
     }
 }
