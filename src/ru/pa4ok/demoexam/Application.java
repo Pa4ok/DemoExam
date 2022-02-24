0c78c3411ef41600df0565af919290be3a990bff
Pa4ok <n_leontiev@mail.ru>
2022-02-24 17:27:20 +0300

    24.02.22 разбор варианта с пересдачи ппа1

--- src/ru/pa4ok/demoexam/Application.java
+++ src/ru/pa4ok/demoexam/Application.java
@@ -12,7 +12,7 @@ import java.sql.SQLException;
  */
 public class Application
 {
-    public static boolean ADMIN_MODE;
+    public static boolean ADMIN_MODE = true;
 
     public static void main(String[] args)
     {
@@ -22,7 +22,7 @@ public class Application
             e.printStackTrace();
         }
 
-        ADMIN_MODE = "0000".equals(JOptionPane.showInputDialog("Введите пароль админа (если знаете)"));
+        //ADMIN_MODE = "0000".equals(JOptionPane.showInputDialog("Введите пароль админа (если знаете)"));
 
         new ProductTableForm();
     }
--- src/ru/pa4ok/demoexam/ui/ProductCreateForm.java
+++ src/ru/pa4ok/demoexam/ui/ProductCreateForm.java
@@ -1,8 +1,12 @@
 package ru.pa4ok.demoexam.ui;
 
+import ru.pa4ok.demoexam.entity.ProductEntity;
+import ru.pa4ok.demoexam.manager.ProductEntityManager;
 import ru.pa4ok.demoexam.util.BaseForm;
+import ru.pa4ok.demoexam.util.DialogUtil;
 
 import javax.swing.*;
+import java.sql.SQLException;
 
 public class ProductCreateForm extends BaseForm
 {
@@ -33,6 +37,70 @@ public class ProductCreateForm extends BaseForm
         backButton.addActionListener(e -> {
             dispose();
             new ProductTableForm();
+        });
+
+        saveButton.addActionListener(e -> {
+            String title = titleField.getText();
+            if(title.isEmpty() || title.length() > 100) {
+                DialogUtil.showError(this, "Название введено неверно");
+                return;
+            }
+
+            String type = typeField.getText();
+            if(type.isEmpty() || type.length() > 100) {
+                DialogUtil.showError(this, "Тип введен неверно");
+                return;
+            }
+
+            String article = articleField.getText();
+            if(article.isEmpty() || article.length() > 10) {
+                DialogUtil.showError(this, "Артикул введен неверно");
+                return;
+            }
+
+            String desc = descArea.getText();
+
+            String image = imageField.getText();
+            if(image.length() > 100) {
+                DialogUtil.showError(this, "Картинка введена неверно");
+                return;
+            }
+
+            int personCount = (int) personCountSpinner.getValue();
+            if(personCount < 0) {
+                DialogUtil.showError(this, "Количество человек для производства введено неверно");
+                return;
+            }
+
+            int workshopNumber = (int) workshopNumberSpinner.getValue();
+            if(workshopNumber < 0) {
+                DialogUtil.showError(this, "Номер рабочей станции введен неверно");
+                return;
+            }
+
+            double cost = -1;
+            try {
+                cost = Double.parseDouble(costField.getText());
+            } catch (Exception ex) {
+                DialogUtil.showError(this, "Цена ввелена неверно");
+                return;
+            }
+            if(cost < 0) {
+                DialogUtil.showError(this, "Цена ввелена неверно");
+                return;
+            }
+
+            ProductEntity product = new ProductEntity(-1, title, type, article, desc, image, personCount, workshopNumber, cost);
+
+            try {
+                ProductEntityManager.insert(product);
+                DialogUtil.showInfo(this, "Запись успешно сохранена");
+                dispose();
+                new ProductTableForm();
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+                DialogUtil.showError(this, "Ошибка сохранения данных: " + ex.getMessage());
+            }
         });
     }
 }
